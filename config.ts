28ef4986749b65cf3ce84ca994437fbbbab0bee8
Em Lazer-Walker <me@lazerwalker.com>
2020-07-22 18:07:00 -0400

    Futz with function settings

---
+++ config.ts
@@ -0,0 +1,5 @@
+// TODO: Eventually abstract this out of this repo
+// My .env trick I've used in other projects needs some touching up of its regex
+export default {
+    SERVER_HOSTNAME: "https://roguelike-mud.azurewebsites.net"
+}
