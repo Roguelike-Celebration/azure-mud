c6260625729ae2e89b1e09ed002f83af5375b668
Kawa <kawaii.dragoness@gmail.com>
2020-08-20 23:56:35 -0400

    Update ThemeSelectorView.tsx
    more linter fixes, mostly about spacing

--- src/components/ThemeSelectorView.tsx
+++ src/components/ThemeSelectorView.tsx
@@ -1,73 +1,72 @@
-import React, {useState, useContext} from 'react'
-import {PublicUser} from '../../server/src/user'
+import React, { useState, useContext } from 'react'
+import { PublicUser } from '../../server/src/user'
 
 import '../../style/profileEditView.css'
-import {DispatchContext} from '../App'
-import {HideModalAction} from '../Actions'
+import { DispatchContext } from '../App'
+import { HideModalAction } from '../Actions'
 
 interface Props {
-    isFTUE: boolean;
-    defaultHandle?: string;
-    user?: PublicUser;
+  isFTUE: boolean;
+  defaultHandle?: string;
+  user?: PublicUser;
 }
 
 export default function ThemeSelectorView (props: Props) {
-    const dispatch = useContext(DispatchContext)
+  const dispatch = useContext(DispatchContext)
 
-    const { defaultHandle, user} = props
+  const { defaultHandle, user} = props
 
-    const close = () => {
-        dispatch(HideModalAction())
-    }
+  const close = () => {
+  dispatch(HideModalAction())
+  }
 
-    // By default, use the state in local storage
-    // Otherwise, set the state in local storage to be Default
+  // By default, use the state in local storage
+  // Otherwise, set the state in local storage to be Default
 
-    // Set the selection of the radio group upon opening the modal
-    const [selectedTheme, setselectedTheme] = React.useState(
-        localStorage.getItem('UserSelectedTheme') || 'Default'
-    );
+  // Set the selection of the radio group upon opening the modal
+  const [selectedTheme, setselectedTheme] = React.useState(
+    localStorage.getItem('UserSelectedTheme') || 'Default'
+  )
 
-    //Handle what happens when you change the modal - 
-    //// change the value in local storage
-    //// then change the actual theme
-    //// then refresh
+  // Handle what happens when you change the modal
+  /// change the value in local storage
+  /// then change the actual theme
+  /// then refresh
 
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setselectedTheme(event.target.value);
-        localStorage.setItem('UserSelectedTheme',event.target.value);
-        window.location.reload();
-    };
-    return(
-        
-        <div className={`container ${props.isFTUE ? 'ftue' : ''}`}>
-        <div className ="form" id="ThemeSelectionForm">
-            <label htmlFor="ThemeSelectionForm">Select Theme:</label>
-                <div className = "radio"><label><input 
-                    type = "radio"
-                    id = "theme"
-                    value = "default"
-                    checked = {selectedTheme === 'default'}
-                    onChange = {handleChange}
-                    />
-                    Default (Dark)</label></div>
-                <div className = "radio"><label><input 
-                    type = "radio"
-                    id = "theme"
-                    value = "solarized-dark"
-                    checked = {selectedTheme === 'solarized-dark'}
-                    onChange = {handleChange}
-                    />
-                    Solarized Dark</label></div>
-                <div className = "radio"><label><input 
-                    type = "radio"
-                    id = "theme"
-                    value = "solarized-light"
-                    checked = {selectedTheme === 'solarized-light'}
-                    onChange = {handleChange}
-                    />
-                    Solarized Light</label></div>
-        </div>
-        </div>
-    )
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setselectedTheme(event.target.value)
+    localStorage.setItem('UserSelectedTheme', event.target.value)
+    window.location.reload()
+  }
+  return (
+    <div className={`container ${props.isFTUE ? 'ftue' : ''}`}>
+    <div className ="form" id="ThemeSelectionForm">
+      <label htmlFor="ThemeSelectionForm">Select Theme:</label>
+      <div className = "radio"><label>
+          <input type = "radio"
+          id = "theme"
+          value = "default"
+          checked = {selectedTheme === 'default'}
+          onChange = {handleChange}
+          />
+          Default (Dark)</label></div>
+      <div className = "radio"><label>
+          <input type = "radio"
+          id = "theme"
+          value = "solarized-dark"
+          checked = {selectedTheme === 'solarized-dark'}
+          onChange = {handleChange}
+          />
+          Solarized Dark</label></div>
+      <div className = "radio"><label>
+          <input type = "radio"
+          id = "theme"
+          value = "solarized-light"
+          checked = {selectedTheme === 'solarized-light'}
+          onChange = {handleChange}
+          />
+          Solarized Light</label></div>
+    </div>
+    </div>
+  )
 }
