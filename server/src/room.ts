64ff4eff58a28382a43502a0e3636870bf80b38a
Em Lazer-Walker <me@lazerwalker.com>
2020-08-25 23:21:16 -0400

    De-GitHub-ify the space

--- server/src/room.ts
+++ server/src/room.ts
@@ -23,43 +23,56 @@ export interface Room {
 export const roomData: { [name: string]: Room } = {
   kitchen: {
     id: 'kitchen',
-    displayName: 'GitHub HQ: Kitchen',
+    displayName: 'Kitchen',
     shortName: 'the kitchen',
-    description: `A series of long picnic tables made of rustic wood abut a stainless steel kitchen island. On the island are a few samovars of Sightglass coffee — don't worry, there's plenty of decaf too — and hot water for tea, plus a few trays of Arizmendi pastries.
+    description: `A series of long picnic tables made of rustic wood abut a stainless steel kitchen island. On the island are a few samovars of coffee — don't worry, there's plenty of decaf too — and hot water for tea, plus a few trays of pastries.
       From here, you can walk over to the [[bar]] or grab a seat in the [[main theatre area->theatre]].`,
     allowsMedia: true,
     hasNoteWall: true
   },
   theatre: {
     id: 'theatre',
-    displayName: 'GitHub HQ: Theatre',
+    displayName: 'Theatre',
     shortName: 'the theatre',
     // kawa: fixed typo, changed src to Twitch per issue #89. Note 'parent' will need to be changed if we change domains, see issue #88. Twitch documentation about 'parent': https://discuss.dev.twitch.tv/t/twitch-embedded-player-updates-in-2020/23956
-    description: 'A stage, confusingly decorated with Halloween skulls and streamers. There are a few dozen flimsy metal chairs you can sit in, plus some comfy couches in the back. You can leave to the [[kitchen]], the [[bar]], or the [[arcade]].<br/><br/><center><iframe width="560" height="315" src="https://player.twitch.tv/?channel=roguelike_con&parent=lazerwalker.com" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></center><br/>'
+    description: 'A stage, confusingly decorated with Halloween skulls and streamers. There are a few dozen flimsy metal chairs you can sit in, plus some comfy couches in the back. You can leave to the [[kitchen]], the [[bar]], the [[lounge]], or clamber into the [[shipping container->shippingContainer]].<br/><br/><center><iframe width="560" height="315" src="https://player.twitch.tv/?channel=roguelike_con&parent=lazerwalker.com" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></center><br/>',
+    allowsMedia: true
   },
   bar: {
     id: 'bar',
-    displayName: 'GitHub HQ: Bar',
+    displayName: 'Bar',
     shortName: 'the bar',
-    description: 'A beautiful long bar with hundreds of bottles spanning up to the ceiling. A friendly bartender will happily make you whatever you want. A laminated sign on the bartop advertises tonight\'s specials: the Tourist (a non-alcoholic drink with lots of fruit and a fun umbrella), the Berlin Interpretation (a mojito made with some sort of hyper-caffeinated soda), and the Walls Are Shifting (a Long Island Iced Tea).<br/><br/>You\'re a stone\'s throw away from the [[kitchen]], the [[theatre]], and the [[arcade]].'
+    description: 'A beautiful long bar with hundreds of bottles spanning up to the ceiling. A friendly bartender will happily make you whatever you want. A laminated sign on the bartop advertises tonight\'s specials: the Tourist (a non-alcoholic drink with lots of fruit and a fun umbrella), the Berlin Interpretation (a mojito made with some sort of hyper-caffeinated soda), and the Walls Are Shifting (a Long Island Iced Tea).<br/><br/>You\'re a stone\'s throw away from the [[kitchen]], the [[theatre]], the [[dance floor]], and the [[lounge]]. You can also crawl into the [[shipping container->shippingContainer]].',
+    allowsMedia: true
   },
-  arcade: {
-    id: 'arcade',
-    displayName: 'GitHub HQ: Arcade Entryway',
-    shortName: 'the arcade',
-    description: `The entryway of the GitHub office has been turned into a makeshift arcade of sorts. Developers are standing at folding tables with laptops, eager to show off their works-in-progress.<br/><br/>
-      You are currently looking at <a href="https://krystman.itch.io/porklike" target="_blank">Porklike</a> by <a href="https://twitter.com/krystman" target="_blank">Krystian Majewski</a>.<br/><br/>
-      
-      <center><iframe src="https://www.lexaloffle.com/bbs/widget.php?pid=porklike" allowfullscreen width="621" height="513" style="border:none; overflow:hidden"></iframe></center><br/><br/>
-      
-      From here, you can get back to the [[bar]] or the [[theatre]].`
+  lounge: {
+    id: 'lounge',
+    displayName: 'Lounge',
+    shortName: 'the lounge',
+    description: 'A chill space to hang away from the hustle and bustle of the main space. Comfy chairs, TVs showing video footage of roguelikes, and a fridge full of La Croix. <br/><br/>From here, you can get to the [[bar]] or the [[theatre]].',
+    allowsMedia: true
   },
-  conference: {
-    id: 'conference',
-    displayName: 'The Executive Suite Conference Room',
-    shortName: 'the conference room',
-    description:
-      "An executive conference room, straight out of a movie: a beautiful mahogany conferenece table, floor-to-ceiling windows overlooking the city, top-of-the-line conference equipment. A poster hangs on the wall expressing Garfield's hatred of Mondays, but it's impeccably framed and very classy.",
+  statue: {
+    id: 'statue',
+    displayName: '@-sign Statue',
+    shortName: 'the statue',
+    description: 'A memorial to countless adventurers who have helped build this social space.<br/><br/>A plaque on the statue shows a list of <a href="https://github.com/lazerwalker/azure-mud/graphs/contributors" target="_blank" rel="noreferrer">code contributors</a>.<br/>There\'s also a suggestion wall for people to add comments about the social space.',
+    hasNoteWall: true,
+    allowsMedia: true
+  },
+  danceFloor: {
+    id: 'danceFloor',
+    displayName: 'Dance Floor',
+    shortName: 'the dance floor',
+    description: 'The ping-pong table has been pushed to the side for a makeshift dance floor. A DJ booth is set up where chiptunes are playing.<br/><br/>From here, you can reach [[the kitchen]] or [[the bar]].'
+  },
+  shippingContainer: {
+    id: 'shippingContainer',
+    displayName: 'Shipping Container',
+    shortName: 'the shipping container',
+    description: `
+      It's not quite clear why there's a shipping container in the middle of the space. Seems pretty chill, though? Somebody's set up a makeshift bench.<br/><br/>
+      After you climb out, you can get back to the [[bar]], the [[theatre]], the [[kitchen]], or the [[lounge]].`,
     allowsMedia: true
   }
 }
