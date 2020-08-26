7d202a3126c4e6b008c45aeea97bc5a2fd98f141
Em Lazer-Walker <me@lazerwalker.com>
2020-08-25 23:07:18 -0400

    Spell 'referrer' correctly

--- src/components/CodeOfConductView.tsx
+++ src/components/CodeOfConductView.tsx
@@ -14,7 +14,7 @@ export default function CodeOfConductView () {
   return (
     <div>
       <p>Our goal is to have a really fun and welcoming celebration of roguelike games, so we have a formalized code of conduct that sets these expectations.</p>
-      <p><a href='https://roguelike.club/code.html' target='_blank' rel='noreferer'>Read our full Code of Conduct on our website.</a></p>
+      <p><a href='https://roguelike.club/code.html' target='_blank' rel='noreferrer'>Read our full Code of Conduct on our website.</a></p>
       <p>For this event, if you&apos;re being harrassed, notice someone being harrassed, or have any other concerns related to the code of conduct, <strong>you can contact one of this event&apos;s organizers.</strong> These people are currently noted by a crown emoji 👑 next to their name. You can also use the <code>/mod</code> command to privately message all moderators.</p>
     </div>
   )
