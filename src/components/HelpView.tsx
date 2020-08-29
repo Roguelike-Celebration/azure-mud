5a89cc2e022657e65cd2b9bbf406a5a2fd7f99d1
Em <me@lazerwalker.com>
2020-08-29 17:03:46 -0400

    Tighten up help screen copy

--- src/components/HelpView.tsx
+++ src/components/HelpView.tsx
@@ -14,18 +14,11 @@ export default function HelpView () {
   return (
     <div>
       <h1>Welcome!</h1>
-      <p>Welcome to the Roguelike Celebration social space! This is a custom-built, mainly chat-based site for Roguelike Celebration 2020.</p>
-      <p>Just remember you can access this reference via the <strong>Help</strong> option in the left sidebar, or via typing <strong>/help</strong> into the chatbox.</p>
-      <p>If you have questions, feedback (we would <em>love</em> feedback on the social space), or need to report a code of conduct violation, contact us via the <strong>/mod</strong> command.</p>
-      <h2>Quick Guide</h2>
-      <ul>
-        <li>Text chat and video chat are done via the center pane</li>
-        <li>You can edit your profile, change the color scheme, or log out by clicking your user name in the upper-left corner</li>
-        <li>The top of the left sidebar contains informational data, such as the map, schedule, and code of conduct</li>
-        <li>You can navigate by clicking Map and selecting a room, clicking the highlighted links in the room description, or using the bottom of the left sidebar</li>
-        <li>You can view the profile or whisper another user by clicking on their name</li>
-      </ul>
-      <h2>Slash Commands Reference</h2>
+      <p>Welcome to the Roguelike Celebration social space! This is an open-source chat app / virtual world built specifically for Roguelike Celebration.</p>
+      <p>You can access this reference at any time by clicking the <strong>Help</strong> button in the left sidebar or by typing <strong>/help</strong> into the chat box.</p>
+      <p>If you have questions, feedback, or need to report a code of conduct violation, you can contact us via the <strong>/mod</strong> command.</p>
+      <h2>Command Reference</h2>
+      <p>You can enter any of these commands by typing directly into the chat box.</p>
       <table>
         {rows}
       </table>
