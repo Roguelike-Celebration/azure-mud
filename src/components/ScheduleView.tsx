3c23374457f48fce4dba8e4df5d6706c1ff480ff
Kawa <kawaii.dragoness@gmail.com>
2020-09-01 16:24:33 -0400

    Moderator changes, restores user counts in sidebar, schedule change
    Issue #196
    * Moderators now have both '[Moderator] ' before their username and are highlighted in a high-contrast way versus normal users
    * This is also reflected in Code of Conduct text
    
    Issue #213
    * Restore user counts on sidebar
    
    Also updated the schedule to not be that of the preview - but will need to be fixed again when we determine speaker order

--- src/components/ScheduleView.tsx
+++ src/components/ScheduleView.tsx
@@ -1,23 +1,49 @@
 import React from 'react'
 
 export default function ScheduleView () {
-  const dateStr = (time) => `2020-08-30T${time}:00.000-07:00`
+  const dateStr = (time) => `2020-10-03T${time}:00.000-07:00`
 
+  // To do: replace with actual speaker names
   const times = [
-    ['16:00', 'Doors Open'],
-    ['16:20', 'Kickoff'],
-    ['16:30', 'Darren Grey'],
-    ['16:40', 'Julian Day'],
-    ['16:50', 'Todd Furmanski'],
-    ['17:00', 'Tabitha Sable'],
-    ['17:10', 'Break'],
-    ['17:20', 'Gabriel Koenig'],
-    ['17:30', 'Cat Manning'],
-    ['17:40', 'Kate Comptom'],
-    ['17:50', 'Break'],
-    ['18:10', 'Preview of "Help Me Steal The Mona Lisa"'],
-    ['19:00', 'Doors Close']
+    ['09:30', 'Intro / Housekeeping'],
+    ['09:30', 'Talk 1'],
+    ['10:30', 'Talk 2'],
+    ['10:30', 'Unconferencing #1'],
+    ['11:30', 'Talk 3'],
+    ['12:30', 'Talk 4'],
+    ['12:30', 'Break'],
+    ['13:30', 'Lightning Talks'],
+    ['14:30', 'Talk 5'],
+    ['15:30', 'Talk 6'],
+    ['15:30', 'Break'],
+    ['16:30', 'Talk 7'],
+    ['16:30', 'Talk 8'],
+    ['17:30', 'Outro / Performances'],
+    ['17:30', 'Game Showcase / Unconferencing #2'],
+    ['18:30', 'Wrap']
   ]
+
+  // Including these here to be swapped in Saturday to Sunday.
+  // Again, needs to be replaced with actual speaker names
+  // const SundayTimes = [
+  //   ['09:30', 'Intro / Housekeeping'],
+  //   ['09:30', 'Lightning Talks'],
+  //   ['10:30', 'Game Showcase / Unconferencing #3'],
+  //   ['11:30', 'Talk 9'],
+  //   ['12:30', 'Talk 10'],
+  //   ['12:30', 'Break'],
+  //   ['13:30', 'Talk 11'],
+  //   ['14:30', 'Talk 12'],
+  //   ['14:30', 'Talk 13'],
+  //   ['15:30', 'Talk 14'],
+  //   ['15:30', 'Break'],
+  //   ['16:30', 'Talk 15'],
+  //   ['16:30', 'Talk 16'],
+  //   ['17:30', 'Lightning Talks'],
+  //   ['18:30', 'Outro / Performances'],
+  //   ['18:30', 'Unconferencing #4'],
+  //   ['19:30', 'Wrap']
+  // ]
 
   const formatter = new Intl.DateTimeFormat('en', { hour: 'numeric', minute: 'numeric' })
 
