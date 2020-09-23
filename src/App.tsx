850850b6403a066fa8dfbded506c49a8f5b24019
Travis Moy <moytravis@gmail.com>
2020-09-22 19:12:48 -0700

    Safe default if "whispers" localStorage key gone
    Either by deletion or by a client who just got pushed the update.

--- src/App.tsx
+++ src/App.tsx
@@ -30,6 +30,7 @@ import HelpView from './components/HelpView'
 import MapModalView from './components/MapModalView'
 import LoggedOutView from './components/LoggedOutView'
 import WelcomeModalView from './components/WelcomeModalView'
+import { WhisperMessage } from './message'
 
 export const DispatchContext = createContext(null)
 export const UserMapContext = createContext(null)
@@ -77,7 +78,7 @@ const App = () => {
           if (localLocalData) {
             try {
               const messages = JSON.parse(rawMessageData)
-              const whispers = JSON.parse(rawWhisperData)
+              const whispers = JSON.parse(rawWhisperData) || new Array<WhisperMessage>()
               dispatch(LoadMessageArchiveAction(messages, whispers))
             } catch (e) {
               console.log('Could not parse message JSON', e)
