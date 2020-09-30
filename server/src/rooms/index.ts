98eb5dcaee66a220603e7cb5a8316d511cfef047
Em Lazer-Walker <me@lazerwalker.com>
2020-09-29 20:08:18 -0400

    Extract out showcase halls into their own files

---
+++ server/src/rooms/index.ts
@@ -0,0 +1,121 @@
+import northShowcaseHall from './northShowcaseHall'
+import southShowcaseHall from './southShowcaseHall'
+import eastShowcaseHall from './eastShowcaseHall'
+import westShowcaseHall from './westShowcaseHall'
+
+export interface NoteWallData {
+  roomWallDescription: string
+  noteWallButton: string
+  addNoteLinkText: string
+  addNotePrompt: string
+  noteWallDescription: string
+}
+
+export interface Room {
+  // e.g. "kitchen"
+  id: string
+
+  // e.g. "GitHub HQ: Kitchen"
+  displayName: string
+
+  // e.g. "the kitchen"
+  shortName: string
+
+  description: string
+
+  // If true, webRTC audio/video chat can happen
+  allowsMedia?: boolean
+
+  // Indicates whether the room should let users place post-it notes
+  // As we add more pieces of one-off functionality,
+  // having a bunch of ad-hoc flags like this will probably get frustrating quickly.
+  // We may want to eventually refactor to something resembling an ECS.
+  hasNoteWall?: boolean
+  noteWallData?: NoteWallData
+
+  // If true, don't show the room in the side list
+  hidden?: boolean
+}
+
+export const roomData: { [name: string]: Room } = {
+  kitchen: {
+    id: 'kitchen',
+    displayName: 'Kitchen',
+    shortName: 'the kitchen',
+    description: `A series of long picnic tables made of rustic wood abut a stainless steel kitchen island. On the island are a few samovars of coffee — don't worry, there's plenty of decaf too — and hot water for tea, plus a few trays of pastries.
+      From here, you can walk over to the [[bar]] or grab a seat in the [[main theater area->theater]].`,
+    allowsMedia: true,
+    hasNoteWall: true
+  },
+  theater: {
+    id: 'theater',
+    displayName: 'Theater',
+    shortName: 'the theater',
+    // kawa: fixed typo, changed src to Twitch per issue #89. Note 'parent' will need to be changed if we change domains, see issue #88. Twitch documentation about 'parent': https://discuss.dev.twitch.tv/t/twitch-embedded-player-updates-in-2020/23956
+    description: 'A stage, confusingly decorated with Halloween skulls and streamers. There are a few dozen flimsy metal chairs you can sit in, plus some comfy couches in the back. You can leave to the [[kitchen]], the [[bar]], the [[lounge]], or clamber into the [[shipping container->shippingContainer]].<br/><br/><center id="iframes"><iframe width="560" height="315" src="https://player.twitch.tv/?channel=roguelike_con&parent=chat.roguelike.club" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><iframe id="captions" width="560" height="100" src="https://www.streamtext.net/player/?event=RoguelikeCelebration&chat=false&header=false&footer=false&indicator=false&ff=Consolas&fgc=93a1a1" frameborder="0" allow="autoplay; encrypted-media;" allowfullscreen></iframe></center><br/><a href="stream.html" onClick="window.open(\'stream.html#\' + window.getComputedStyle(document.body).getPropertyValue(\'background-color\'), \'stream\', \'width=560,height=450\'); return false">Pop Out Stream</a><br/>',
+    allowsMedia: true,
+    hasNoteWall: true,
+    noteWallData: {
+      roomWallDescription: 'There is a whiteboard set up to the side with "SPEAKER QUESTIONS" written at the top. "Questions for speakers not questions from speakers!" is hastily scrawled below it.',
+      noteWallButton: 'Write a question',
+      addNoteLinkText: 'add a question',
+      addNotePrompt: 'What would you like to ask?',
+      noteWallDescription: 'Questions for the current speaker, ranked by upvotes.'
+    }
+  },
+  bar: {
+    id: 'bar',
+    displayName: 'Bar',
+    shortName: 'the bar',
+    description: 'A beautiful long bar with hundreds of bottles spanning up to the ceiling. A friendly bartender will happily make you whatever you want. A laminated sign on the bartop advertises tonight\'s specials: the Tourist (a non-alcoholic drink with lots of fruit and a fun umbrella), the Berlin Interpretation (a mojito made with some sort of hyper-caffeinated soda), and the Walls Are Shifting (a Long Island Iced Tea).<br/><br/>You\'re a stone\'s throw away from the [[kitchen]], the [[theater]], the [[dance floor->danceFloor]], and the [[lounge]]. You can also crawl into the [[shipping container->shippingContainer]].',
+    allowsMedia: true
+  },
+  lounge: {
+    id: 'lounge',
+    displayName: 'Lounge',
+    shortName: 'the lounge',
+    description: 'A chill space to hang away from the hustle and bustle of the main space. Comfy chairs, TVs showing video footage of roguelikes, and a fridge full of La Croix. <br/><br/>From here, you can get to the [[bar]] or the [[theater]].',
+    allowsMedia: true
+  },
+  statue: {
+    id: 'statue',
+    displayName: '@-sign Statue',
+    shortName: 'the statue',
+    description: 'A memorial to countless adventurers who have helped build this social space.<br/><br/>A plaque on the statue shows a list of <a href="https://github.com/lazerwalker/azure-mud/graphs/contributors" target="_blank" rel="noreferrer">code contributors</a>.<br/>There\'s also a suggestion wall for people to add comments about the social space.',
+    hasNoteWall: true,
+    allowsMedia: true
+  },
+  danceFloor: {
+    id: 'danceFloor',
+    displayName: 'Dance Floor',
+    shortName: 'the dance floor',
+    description: 'The ping-pong table has been pushed to the side for a makeshift dance floor. A DJ booth is set up where chiptunes are playing.<br/><br/>From here, you can reach [[the kitchen]] or [[the bar]].'
+  },
+  shippingContainer: {
+    id: 'shippingContainer',
+    displayName: 'Shipping Container',
+    shortName: 'the shipping container',
+    description: `
+      It's not quite clear why there's a shipping container in the middle of the space. Seems pretty chill, though? Somebody's set up a makeshift bench.<br/><br/>
+      After you climb out, you can get back to the [[bar]], the [[theater]], the [[kitchen]], or the [[lounge]].`,
+    allowsMedia: true
+  },
+  entryway: {
+    id: 'entryway',
+    displayName: 'Registration Desk',
+    shortName: 'the registration desk',
+    description: 'A big banner reads welcome to Roguelike Celebration! Once you\'ve got your bearings about you, you can move to the [[lounge]].',
+    hidden: true
+  },
+  foyer: {
+    id: 'entryway',
+    displayName: 'Registration Desk',
+    shortName: 'the registration desk',
+    description: 'A big banner reads welcome to Roguelike Celebration! Once you\'ve got your bearings about you, you can move to the [[lounge]].',
+    hidden: true
+  },
+  northShowcaseHall,
+  eastShowcaseHall,
+  southShowcaseHall,
+  westShowcaseHall
+}
