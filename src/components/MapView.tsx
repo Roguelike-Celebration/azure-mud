dfd5be6f115b4f968f1e78abf1852366ca13e345
Em Lazer-Walker <me@lazerwalker.com>
2020-09-29 20:35:49 -0400

    Update comments on the map

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -7,22 +7,36 @@ import { HideModalAction } from '../Actions'
 import { DispatchContext } from '../App'
 
 /**
- * This renders a clickable ASCII map! This code is super janky in many ways.
+ * This renders a clickable ASCII map!
  *
- * At a high level, while there's some procedurality in creating the rooms,
- * the overall map is completely declarative. There's a data blob in this file
- * that contains exactly where each room should go, as well as the paths between
- * rooms (which I would *love* to procgen, but seems like a bunch of work)
+ * The map itself is just plaintext ASCII pasted in here, generated using the
+ * Mac app MonoDraw app (see the map.monopic file in the root of this repo)
  *
- * The paths in particular are janky. They have a solid background to override
- * the wall tiles in the case of doors, but that means we ofteh need to chunk out
- * individual paths into multiple ones so that the div being created doesn't
- * unintentionally sit on top of other room tiles and wipe them out. Sigh.
+ * There are two additional data sources in here:
+ *
+ * 1. To make rooms clickable, there is an array of ClickableArea objects
+ * that define clickable squares that move to a given roomId.
+ * Coordinates are given using ASCII character coordinates, not pixels.
+ *
+ * 2. To add real-time presence numbers to rooms, there's an array of roomIds
+ * that correspond to the number in parenthesis that lives in the ASCII map itself
+ * This number is monotonically increasing. To add a new room, add its name to the
+ * end of the array and then make the number you use in the ASCII map the array index + 1
+ * (because the ASCII map numbers are 1-indexed)
  */
+
 interface Props {
     roomData: { [roomId: string]: Room };
     currentRoomId: string
 }
+
+  interface ClickableArea {
+    x: number,
+    y: number,
+    height: number,
+    width: number,
+    roomId: string
+  }
 
 export default function MapView (props: Props) {
   const dispatch = useContext(DispatchContext)
@@ -37,7 +51,54 @@ export default function MapView (props: Props) {
     }
   }, [])
 
-  let mapText = `                ┌────────────────────────┐
+  // mapText = mapText.replace('$0', `(${props.roomData.danceFloor.users.length})`)
+
+  presenceMapping.forEach((roomId, idx) => {
+    let replaceString = '(0)'
+
+    if (props.roomData[roomId] && props.roomData[roomId].users && props.roomData[roomId].users.length > 0) {
+      replaceString = `(${props.roomData[roomId].users.length})`
+    }
+
+    replaceString = replaceString.padEnd(4, '.')
+
+    mapText = mapText.replace(`(${idx.toString().padStart(2, '0')})`, replaceString)
+  })
+
+  const handleClick = (e) => {
+    const roomId =
+      e.target && e.target.getAttribute && e.target.getAttribute('data-room')
+    if (roomId) {
+      moveToRoom(roomId)
+      dispatch(HideModalAction())
+    }
+  }
+
+  const clickableDivs = clickableAreas.map(a => {
+    return <div
+      style={{
+        position: 'absolute',
+        left: `${a.x * 10}px`,
+        top: `${a.y * 15}px`,
+        width: `${a.width * 10}px`,
+        height: `${a.height * 15}px`,
+        cursor: 'pointer'
+      }}
+      key={a.roomId}
+      onClick={handleClick}
+      data-room={a.roomId}
+      id={`clickable-room-${a.roomId}`} />
+  })
+
+  return <div style={{ position: 'relative', margin: '15px' }}>
+    {clickableDivs}
+    <pre style={{ letterSpacing: '2px', height: '500px' }}><code>
+      {mapText}
+    </code></pre>
+  </div>
+}
+
+let mapText = `                ┌────────────────────────┐
 ┌──────────┐    │........................│                    ┌─────────────────────────┐
 │..........│    │......Kitchen (01)......│                    │.........................│
 │..Quiet ..│    │........................│                    │.........................│
@@ -79,214 +140,161 @@ export default function MapView (props: Props) {
                              │..............................│
                              └──────────────────────────────┘
    `
-  // mapText = mapText.replace('$0', `(${props.roomData.danceFloor.users.length})`)
 
-  const presenceMapping = [
-    'danceFloor',
-    'kitchen',
-    'kitchenTableB',
-    'kitchenTableA',
-    'kitchenTableC',
-    'bar',
-    'lounge',
-    'shippingContainer',
-    'theater',
-    'atelier',
-    'statue',
-    'northShowcaseHall',
-    'eastShowcaseHall',
-    'westShowcaseHall',
-    'southShowcaseHall',
-    'study',
-    'swag',
-    'foyer',
-    'workbench'
-  ]
+const presenceMapping = [
+  'danceFloor',
+  'kitchen',
+  'kitchenTableB',
+  'kitchenTableA',
+  'kitchenTableC',
+  'bar',
+  'lounge',
+  'shippingContainer',
+  'theater',
+  'atelier',
+  'statue',
+  'northShowcaseHall',
+  'eastShowcaseHall',
+  'westShowcaseHall',
+  'southShowcaseHall',
+  'study',
+  'swag',
+  'foyer',
+  'workbench'
+]
 
-  presenceMapping.forEach((roomId, idx) => {
-    let replaceString = '(0)'
-
-    if (props.roomData[roomId] && props.roomData[roomId].users && props.roomData[roomId].users.length > 0) {
-      replaceString = `(${props.roomData[roomId].users.length})`
-    }
-
-    replaceString = replaceString.padEnd(4, '.')
-
-    mapText = mapText.replace(`(${idx.toString().padStart(2, '0')})`, replaceString)
-  })
-
-  interface ClickableArea {
-    x: number,
-    y: number,
-    height: number,
-    width: number,
-    roomId: string
+const clickableAreas: ClickableArea[] = [
+  {
+    roomId: 'lounge',
+    x: 0,
+    y: 1,
+    width: 12,
+    height: 8
+  },
+  {
+    roomId: 'danceFloor',
+    x: 1,
+    y: 10,
+    width: 11,
+    height: 10
+  },
+  {
+    roomId: 'kitchen',
+    x: 16,
+    y: 0,
+    width: 26,
+    height: 15
+  },
+  {
+    roomId: 'kitchenTableA',
+    x: 17,
+    y: 10,
+    height: 3,
+    width: 11
+  },
+  {
+    roomId: 'kitchenTableB',
+    x: 20,
+    y: 5,
+    height: 3,
+    width: 11
+  },
+  {
+    roomId: 'kitchenTableC',
+    x: 26,
+    y: 8,
+    width: 11,
+    height: 3
+  },
+  {
+    roomId: 'bar',
+    x: 16,
+    y: 15,
+    width: 25,
+    height: 7
+  },
+  {
+    roomId: 'shippingContainer',
+    x: 44,
+    y: 4,
+    width: 12,
+    height: 5
+  },
+  {
+    roomId: 'statue',
+    x: 45,
+    y: 12,
+    width: 10,
+    height: 7
+  },
+  {
+    roomId: 'theater',
+    x: 60,
+    y: 1,
+    width: 27,
+    height: 26
+  },
+  {
+    roomId: 'foyer',
+    x: 61,
+    y: 28,
+    width: 21,
+    height: 8
+  },
+  {
+    roomId: 'swag',
+    x: 82,
+    y: 28,
+    width: 11,
+    height: 8
+  },
+  {
+    roomId: 'workbench',
+    x: 0,
+    y: 23,
+    width: 14,
+    height: 8
+  },
+  {
+    roomId: 'atelier',
+    x: 15,
+    y: 23,
+    width: 12,
+    height: 8
+  },
+  {
+    roomId: 'study',
+    x: 2,
+    y: 31,
+    width: 24,
+    height: 7
+  },
+  {
+    roomId: 'northShowcaseHall',
+    x: 35,
+    y: 22,
+    width: 17,
+    height: 6
+  },
+  {
+    roomId: 'southShowcaseHall',
+    x: 35,
+    y: 36,
+    width: 17,
+    height: 4
+  },
+  {
+    roomId: 'westShowcaseHall',
+    x: 45,
+    y: 28,
+    width: 15,
+    height: 8
+  },
+  {
+    roomId: 'eastShowcaseHall',
+    x: 28,
+    y: 28,
+    width: 15,
+    height: 8
   }
-  const clickableAreas: ClickableArea[] = [
-    {
-      roomId: 'lounge',
-      x: 0,
-      y: 1,
-      width: 12,
-      height: 8
-    },
-    {
-      roomId: 'danceFloor',
-      x: 1,
-      y: 10,
-      width: 11,
-      height: 10
-    },
-    {
-      roomId: 'kitchen',
-      x: 16,
-      y: 0,
-      width: 26,
-      height: 15
-    },
-    {
-      roomId: 'kitchenTableA',
-      x: 17,
-      y: 10,
-      height: 3,
-      width: 11
-    },
-    {
-      roomId: 'kitchenTableB',
-      x: 20,
-      y: 5,
-      height: 3,
-      width: 11
-    },
-    {
-      roomId: 'kitchenTableC',
-      x: 26,
-      y: 8,
-      width: 11,
-      height: 3
-    },
-    {
-      roomId: 'bar',
-      x: 16,
-      y: 15,
-      width: 25,
-      height: 7
-    },
-    {
-      roomId: 'shippingContainer',
-      x: 44,
-      y: 4,
-      width: 12,
-      height: 5
-    },
-    {
-      roomId: 'statue',
-      x: 45,
-      y: 12,
-      width: 10,
-      height: 7
-    },
-    {
-      roomId: 'theater',
-      x: 60,
-      y: 1,
-      width: 27,
-      height: 26
-    },
-    {
-      roomId: 'foyer',
-      x: 61,
-      y: 28,
-      width: 21,
-      height: 8
-    },
-    {
-      roomId: 'swag',
-      x: 82,
-      y: 28,
-      width: 11,
-      height: 8
-    },
-    {
-      roomId: 'workbench',
-      x: 0,
-      y: 23,
-      width: 14,
-      height: 8
-    },
-    {
-      roomId: 'atelier',
-      x: 15,
-      y: 23,
-      width: 12,
-      height: 8
-    },
-    {
-      roomId: 'study',
-      x: 2,
-      y: 31,
-      width: 24,
-      height: 7
-    },
-    {
-      roomId: 'northShowcaseHall',
-      x: 35,
-      y: 22,
-      width: 17,
-      height: 6
-    },
-    {
-      roomId: 'southShowcaseHall',
-      x: 35,
-      y: 36,
-      width: 17,
-      height: 4
-    },
-    {
-      roomId: 'westShowcaseHall',
-      x: 45,
-      y: 28,
-      width: 15,
-      height: 8
-    },
-    {
-      roomId: 'eastShowcaseHall',
-      x: 28,
-      y: 28,
-      width: 15,
-      height: 8
-    }
-  ]
-
-  const handleClick = (e) => {
-    const roomId =
-      e.target && e.target.getAttribute && e.target.getAttribute('data-room')
-    if (roomId) {
-      moveToRoom(roomId)
-      dispatch(HideModalAction())
-    }
-  }
-
-  const clickableDivs = clickableAreas.map(a => {
-    return <div
-      style={{
-        position: 'absolute',
-        left: `${a.x * 10}px`,
-        top: `${a.y * 15}px`,
-        width: `${a.width * 10}px`,
-        height: `${a.height * 15}px`,
-        cursor: 'pointer'
-      }}
-      key={a.roomId}
-      onClick={handleClick}
-      data-room={a.roomId}
-      id={`clickable-room-${a.roomId}`} />
-  })
-
-  return <div style={{ position: 'relative', margin: '15px' }}>
-    {clickableDivs}
-    <pre style={{ letterSpacing: '2px', height: '500px' }}><code>
-      {mapText}
-    </code></pre>
-  </div>
-}
+]
