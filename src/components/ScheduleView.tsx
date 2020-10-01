d9ef9bb74b16be70a8a5dbdc75e1d5bbcb6d5cec
Travis Moy <moytravis@gmail.com>
2020-09-30 22:02:28 -0700

    Set up Sunday too

--- src/components/ScheduleView.tsx
+++ src/components/ScheduleView.tsx
@@ -43,31 +43,30 @@ export const ScheduleEntries = [
   ScheduleEntry('19:30', 1, 'Doors Close')
 ]
 
+// Including these here to be swapped in Saturday to Sunday.
+// const ScheduleEntries = [
+//   ScheduleEntry('09:15', 2, 'Intro / Housekeeping', ['theater']),
+//   ScheduleEntry('09:30', 2, 'Lightning Talks 2: Xalavier Nelson Jr., Max Kreminski, Clarissa Littler, Nicholas Feinberg, Tanya X. Short', ['theater']),
+//   ScheduleEntry('10:30', 2, 'Game Showcase / Unconferencing #3', ['northShowcaseHall', 'eastShowcaseHall', 'southShowcaseHall','westShowcaseHall', 'unconference']),
+//   ScheduleEntry('11:00', 2, 'The Game Band: A Mysterious Blaseball Spectacular', ['theater']),
+//   ScheduleEntry('11:30', 2, 'Cat Manning: How To Build A Character System That Doesn\'t Fall Apart Two Turns Later (with apologies to PKD)', ['theater']),
+//   ScheduleEntry('12:00', 2, 'Gabriel Koenig: Good Mutation/Bad Mutation: Player Agency in Procedural Generation', ['theater']),
+//   ScheduleEntry('12:30', 2, 'Break'),
+//   ScheduleEntry('13:30', 2, 'Caelyn Sandel: Teaching the Fun of Losing', ['theater']),
+//   ScheduleEntry('14:00', 2, 'Ivy Melinda: A flower in the garden: cultivating a community for Caves of Qud', ['theater']),
+//   ScheduleEntry('14:30', 2, 'Kate Compton: Making Polite Programming Languages: How to Design a Generative Language without a Programming Language Degree', ['theater']),
+//   ScheduleEntry('15:00', 2, 'Aaron A. Reed: Cadences, Lacunae, and Subcutaneans: Ten Years of Procedural Novels', ['theater']),
+//   ScheduleEntry('15:30', 2, 'Break'),
+//   ScheduleEntry('16:00', 2, 'Julian Day: Poetry at the Edge of Roguelikes: Writing Around Iterative Media', ['theater']),
+//   ScheduleEntry('16:30', 2, 'Todd Furmanski: Mysty Roguelikes, or: Using First Person Point-and-Click Paradigms with Realtime Graphics and Simulation', ['theater']),
+//   ScheduleEntry('17:00', 2, 'Lightning Talks 3: Lee Tusman, Alexander Martin, Josh Grams, Adrian Herbez, Younès Rabii, Duke Dougal' ['theater']),
+//   ScheduleEntry('18:00', 2, 'Leif Bloomquist: Multiplayer Roguelike Gaming!', ['theater']),
+//   ScheduleEntry('18:30', 2, 'Closing Announcements', ['theater']),
+//   ScheduleEntry('18:35', 2, 'Unconferencing #4', ['unconference']),
+//   ScheduleEntry('19:30', 2, 'Wrap)]
+// ]
+
 export default function ScheduleView () {
-
-  // Including these here to be swapped in Saturday to Sunday.
-  // const SundayTimes = [
-  //   ['09:15', 'Intro / Housekeeping'],
-  //   ['09:30', 'Lightning Talks 2: Xalavier Nelson Jr., Max Kreminski, Clarissa Littler, Nicholas Feinberg, Tanya X. Short'],
-  //   ['10:30', 'Game Showcase / Unconferencing #3'],
-  //   ['11:00', 'The Game Band: A Mysterious Blaseball Spectacular'],
-  //   ['11:30', 'Cat Manning: How To Build A Character System That Doesn\'t Fall Apart Two Turns Later (with apologies to PKD)'],
-  //   ['12:00', 'Gabriel Koenig: Good Mutation/Bad Mutation: Player Agency in Procedural Generation'],
-  //   ['12:30', 'Break'],
-  //   ['13:30', 'Caelyn Sandel: Teaching the Fun of Losing'],
-  //   ['14:00', 'Ivy Melinda: A flower in the garden: cultivating a community for Caves of Qud'],
-  //   ['14:30', 'Kate Compton: Making Polite Programming Languages: How to Design a Generative Language without a Programming Language Degree'],
-  //   ['15:00', 'Aaron A. Reed: Cadences, Lacunae, and Subcutaneans: Ten Years of Procedural Novels'],
-  //   ['15:30', 'Break'],
-  //   ['16:00', 'Julian Day: Poetry at the Edge of Roguelikes: Writing Around Iterative Media'],
-  //   ['16:30', 'Todd Furmanski: Mysty Roguelikes, or: Using First Person Point-and-Click Paradigms with Realtime Graphics and Simulation'],
-  //   ['17:00', 'Lightning Talks 3: Lee Tusman, Alexander Martin, Josh Grams, Adrian Herbez, Younès Rabii, Duke Dougal'],
-  //   ['18:00', 'Leif Bloomquist: Multiplayer Roguelike Gaming!'],
-  //   ['18:30', 'Closing Announcements'],
-  //   ['18:35', 'Unconferencing #4'],
-  //   ['19:30', 'Wrap']
-  // ]
-
   const formatter = new Intl.DateTimeFormat('en', { hour: 'numeric', minute: 'numeric' })
 
   const rows = ScheduleEntries.map(r => {
