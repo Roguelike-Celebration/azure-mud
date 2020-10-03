b2fa614c5888dad3ff3eee3e62ae716903296a41
Em Lazer-Walker <me@lazerwalker.com>
2020-10-03 10:21:23 -0400

    Reset server URL

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Eventually abstract this out of this repo
 // My .env trick I've used in other projects needs some touching up of its regex
 export default {
-  SERVER_HOSTNAME: 'https://roguelikecelebration.azurewebsites.net'
+  SERVER_HOSTNAME: 'https://roguelikecelebration-mud.azurewebsites.net'
 }
