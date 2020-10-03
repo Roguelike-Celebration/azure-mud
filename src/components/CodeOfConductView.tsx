2fda41965ed2cb1fc71a4c503027d9b2fe9991fa
Britta <brittag@gmail.com>
2020-10-02 20:24:48 -0700

    Fix typo in CoC

--- src/components/CodeOfConductView.tsx
+++ src/components/CodeOfConductView.tsx
@@ -5,7 +5,7 @@ export default function CodeOfConductView () {
     <div id='code-of-conduct'>
       <p>Our goal is to have a fun and welcoming celebration of roguelike games, so we have a formalized code of conduct that sets these expectations.</p>
       <p><a href='https://roguelike.club/code.html' target='_blank' rel='noreferrer'>Read our Code of Conduct.</a></p>
-      <p>If you&apos;re being harrassed, notice someone being harrassed, or have any other concerns related to the code of conduct, you have a few options:</p>
+      <p>If you&apos;re being harassed, notice someone being harassed, or have any other concerns related to the code of conduct, you have a few options:</p>
       <ul>
         <li>Type <code>/mod</code> into the chat box, followed by a message, to contact all moderators.</li>
         <li>Email <a href="mailto:contact@roguelike.club">contact@roguelike.club</a>.</li>
