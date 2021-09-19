a95148d236b2649edd3c57a2df28a13d8e409201
MoyTW <moytravis@gmail.com>
2021-09-18 23:03:37 -0700

    server decodes token

--- server/firebaseTest/index.ts
+++ server/firebaseTest/index.ts
@@ -1,21 +1,51 @@
 import { AzureFunction, Context, HttpRequest } from '@azure/functions'
 import * as admin from 'firebase-admin'
 
+// TODO: write up that you need to generate firebase key json & store it & set the GOOGLE_APPLICATION_CREDENTIALS
+// env var as per https://firebase.google.com/docs/admin/setup#linux-or-macos
+// Then, when you get the file, stick it in `firebase-admin.json` in the `/server` project directory.
+// Therefore:
+// GOOGLE_APPLICATION_CREDENTIALS = ./firebase-admin.json
+
 const httpTrigger: AzureFunction = async function (
   context: Context,
   req: HttpRequest
 ): Promise<any> {
-  context.log('In Firebase')
   var clientIdToken = req.body.token
+  context.log('In Firebase, token: ' + req.body.token)
 
-  context.res = {
-    status: 200,
-    body: {
-      words: 'words words words',
-      reqBody: req.body,
-      reqFull: req
-    }
+  // Apparently, the initialization persists across function calls (!?) which I didn't realize was something that
+  // could happen! Clearly I don't quite understand 'serverless' (gosh I resent that term! it's still on somebody's
+  // server!)
+  if (admin.apps.length === 0) {
+    admin.initializeApp({
+      credential: admin.credential.applicationDefault()
+    })
   }
+
+  // If we successfully decode the token, we want to cache it.
+  // TODO: cache token
+  // TODO: check expiry on token
+  await admin.auth().verifyIdToken(clientIdToken).then((decoded) => {
+    const uid = decoded.uid
+
+    context.res = {
+      status: 200,
+      body: {
+        decoded: decoded,
+        decodedUid: uid,
+        reqBody: req.body
+      }
+    }
+  }).catch((error) => {
+    context.log('Error authenticating token: ' + clientIdToken + ' error: ' + error)
+    context.res = {
+      status: 400,
+      body: {
+        description: 'Token could not be authenticated!'
+      }
+    }
+  })
 }
 
 export default httpTrigger
