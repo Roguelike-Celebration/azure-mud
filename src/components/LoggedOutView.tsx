2849fdb4a69346e538d22756469f07b0f21a1d95
Travis Moy <moytravis@gmail.com>
2021-09-27 15:53:34 -0700

    Remove old login links

--- src/components/LoggedOutView.tsx
+++ src/components/LoggedOutView.tsx
@@ -3,14 +3,10 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
 import React from 'react'
-import config from '../config'
-const axios = require('axios').default
 
 const uiConfig = {
   // Popup signin flow rather than redirect flow.
   signInFlow: 'popup',
-  // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-  // signInSuccessUrl: '/signedIn',
   callbacks: {
     // The documentation on the firebaseui README appears totally borked at time of writing; the structure of
     // AuthResult doesn't line up with itself! If you go back to that README treat it with caution.
@@ -18,8 +14,6 @@ const uiConfig = {
       console.log('Sign-in successful!')
     }
   },
-  // privacyPolicyUrl: '/privacyPolicy',
-  // tosUrl: '/tosUrl',
   signInOptions: [
     firebase.auth.EmailAuthProvider.PROVIDER_ID,
     firebase.auth.GoogleAuthProvider.PROVIDER_ID
@@ -30,7 +24,7 @@ export default function LoggedOutView () {
   return (
     <div>
       <header role="banner">
-        <h1>!hm.</h1>
+        <h1>Welcome to Roguelike Celebration 2021!</h1>
       </header>
       <main role="main">
         <p>
@@ -42,24 +36,6 @@ export default function LoggedOutView () {
           everyone else!
         </p>
         <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
-        <a
-          href={`${
-            config.SERVER_HOSTNAME
-          }/.auth/login/twitter?post_login_redirect_url=${encodeURIComponent(
-            window.location.href
-          )}`}
-        >
-          Log In With Twitter
-        </a><br/>
-        <a
-          href={`${
-            config.SERVER_HOSTNAME
-          }/.auth/login/google?post_login_redirect_url=${encodeURIComponent(
-            window.location.href
-          )}`}
-        >
-          Log In With Google
-        </a><br/>
         {(window as any).safari
           ? <p>
             <strong style={{ color: 'red' }}> You appear to be using Safari.</strong> <br/>
@@ -72,6 +48,9 @@ export default function LoggedOutView () {
             If you're not comfortable with this, feel free to switch to Chrome, Firefox, or another non-Safari browser on your Mac.
           </p> : null
         }
+        <p>
+          If you use email, we will require you to authenticate the email.
+        </p>
         <p>
           No matter which service you log in with, we do not access any data
           other than what is needed to authenticate you. If it would make you
