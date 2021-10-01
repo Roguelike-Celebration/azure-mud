403d5c6631a7f9e6e8fff81546d4c1f30f45c82e
Travis Moy <moytravis@gmail.com>
2021-10-01 14:19:56 -0700

    Use window.location.href instead of FRONTEND_URL

--- src/firebaseUtils.ts
+++ src/firebaseUtils.ts
@@ -5,7 +5,7 @@ import 'firebase/auth'
 const actionCodeSettings = {
   // This URL should be changed to the frontend location for production. It must also be added to the authorized
   // domains list in the Firebase Console.
-  url: config.FRONTEND_URL,
+  url: window.location.href,
   // This must be true, you'll get an error if it's not.
   handleCodeInApp: true
 };
