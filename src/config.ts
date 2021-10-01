ae195441fe9f61e0048cda36301067abde3f5387
Travis Moy <MoyTW@users.noreply.github.com>
2021-09-30 17:16:29 -0700

    Update config.ts with empty values

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,11 @@
 // TODO: Eventually abstract this out of this repo
 // My .env trick I've used in other projects needs some touching up of its regex
 export default {
-  SERVER_HOSTNAME: 'https://roguelikecelebration-mud.azurewebsites.net'
+  SERVER_HOSTNAME: 'https://roguelikecelebration-mud.azurewebsites.net',
+  FIREBASE_API_KEY: 'INSERT FROM FIREBASE CONSOLE',
+  FIREBASE_AUTH_DOMAIN: 'INSERT FROM FIREBASE CONSOLE',
+  FIREBASE_PROJECT_ID: 'INSERT FROM FIREBASE CONSOLE',
+  FIREBASE_STORAGE_BUCKET: 'INSERT FROM FIREBASE CONSOLE',
+  FIREBASE_MESSAGING_SENDER_ID: 'INSERT FROM FIREBASE CONSOLE',
+  FIREBASE_APP_ID: 'INSERT FROM FIREBASE CONSOLE'
 }
