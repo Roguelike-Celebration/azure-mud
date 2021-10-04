2a557befda1657bfab37e72d65ceee4742fea80b
Kawa <kawaii.dragoness@gmail.com>
2021-10-03 22:20:30 -0400

    Update unconfRooms.ts
    changing the theme, removing the zoom rooms

--- server/src/rooms/unconfRooms.ts
+++ server/src/rooms/unconfRooms.ts
@@ -1,8 +1,11 @@
+// Goal of 2021 conference room names:
+// all 'monsters' that are clearly monstrous/not too humanoid, all start with different letters, come from different trad-roguelikes. But I'm not tied to these.
+
 export const unconference = {
   id: 'unconference',
   displayName: 'Unconferencing Lobby',
   shortName: 'the unconferencing lobby',
-  description: 'Winding corridors lead to a large dungeon-like room. Sticky notes and magic markers are piled up on tables along with assorted adventuring gear. White banners with blocky black text label several hallways leading to the unconferencing rooms - [[Minetown->minetown]], [[Oracle->oracle]], [[Sokoban->sokoban]], [[Castle->castle]], [[Vlad\'s Tower->tower]], and [[Astral Plane->astralPlane]]. You can also return back to the [[theater]].',
+  description: 'Winding corridors lead to a large dungeon-like room. Sticky notes and magic markers are piled up on tables along with assorted adventuring gear. White banners with blocky black text label several hallways leading to the unconferencing rooms - [[cockatrice]], [[dragon]], [[naga]], [[skeleton]], [[tengu]], and [[yak]]. You can also return back to the [[theater]].',
   hasNoteWall: true,
   noteWallData: {
     roomWallDescription: 'One wall of this room is taken up by a large whiteboard titled "UNCONFERENCE TOPIC SUGGESTIONS!" Smaller font clarifies "Write what you want to chat with others about, and upvote topics you find interesting. Moderators will assign the top six topics rooms, 5 minutes into each unconference block. Have fun!"',
@@ -13,50 +16,51 @@ export const unconference = {
   }
 }
 
-export const minetown = {
-  id: 'minetown',
-  displayName: 'Unconferencing: Minetown',
-  shortName: 'minetown',
-  description: 'The clink of picks and sound of distant crashing rock forms a percussive backbeat to this underground town square. Dozens of candles are set into the walls, casting the space in a warm glow. A fountain stands in the center of the square, along with a large sign reading "NO SPLASHING"<br/><br/>We encourage you to use this space to talk "face-to-face" using <a href="https://us02web.zoom.us/j/87156694099?pwd=bVN4WHZTeG9NREk0VFMwQk9DRExaZz09" target="_blank">the Zoom meeting for this room</a>, which you can access at any time during the conference. (Passcode 302165)<br/><br/>This is a room for unconferencing! The other unconferencing rooms are [[Oracle->oracle]], [[Castle->castle]], [[Vlad\'s Tower->tower]], [[Sokoban->sokoban]], and [[Astral Plane->astralPlane]]. Or you can climb the stairs back up to the [[unconferencing lobby->unconference]].',
+export const cockatrice = {
+  id: 'cockatrice',
+  displayName: 'Unconferencing: cockatrice',
+  shortName: 'cockatrice',
+  description: 'This is an unconferencing room. Careful not to get petrified!<br/><br/>This is a room for unconferencing! The other unconferencing rooms are [[dragon]], [[naga]], [[skeleton]], [[tengu]], and [[yak]]. Or you can climb the stairs back up to the [[unconferencing lobby->unconference]].',
   hasNoteWall: true
 }
 
-export const oracle = {
-  id: 'oracle',
-  displayName: 'Unconferencing: Oracle',
-  shortName: 'oracle',
-  description: 'This peaceful space is decorated with burbling fountains, stately marble columns, and finely carved statues of centaurs in various poses. A small temple is the focal point, but the doors are currently closed and barred, with a stern note on the door reading "The oracle is in no mood for consultations." A smaller, more hastily written sign underneath, reads "Tea time @ Vlad\'s, be back soon!"<br/><br/>We encourage you to use this space to talk "face-to-face" using <a href="https://us02web.zoom.us/j/85830277530?pwd=RDNFTnNMSFdhVExya0pYOUJRRDNOUT09" target="_blank">the Zoom meeting for this room</a>, which you can access at any time during the conference. (Passcode 984338)<br/><br/>This is a room for unconferencing! The other unconferencing rooms are [[Minetown->minetown]], [[Castle->castle]], [[Vlad\'s Tower->tower]], [[Sokoban->sokoban]], and [[Astral Plane->astralPlane]]. Or you can go back to the [[unconferencing lobby->unconference]].',
+export const dragon = {
+  id: 'dragon',
+  displayName: 'Unconferencing: dragon\'s lair',
+  shortName: 'dragon\'s lair',
+  description: 'This is an unconferencing room. Careful not to get lost in the horde!<br/><br/>This is a room for unconferencing! The other unconferencing rooms are [[cockatrice]], [[naga]], [[skeleton]], [[tengu]], and [[yak]]. Or you can climb the stairs back up to the [[unconferencing lobby->unconference]].',
   hasNoteWall: true
 }
 
-export const tower = {
-  id: 'tower',
-  displayName: 'Unconferencing: Vlad\'s Tower',
-  shortName: 'Vlad\'s Tower',
-  description: 'Squeaking bats and howling wolves give a festive October ambiance to this gothic tower, as you stand in the center courtyard. Small niches holding antique weaponry or suits of armour surround the yard, which can function as uncomfortably crowded break out rooms in a pinch. From far above you can hear two voices laughing, one Greek and one Transylvanian - but the reclining dragon by the stairs opens a warning eye if you venture too close.<br/><br/>We encourage you to use this space to talk "face-to-face" using <a href="https://us02web.zoom.us/j/83018980256?pwd=R21NU2lsV2UxTXRKeW52WU43bGJwUT09" target="_blank">the Zoom meeting for this room</a>, which you can access at any time during the conference. (Passcode 955148)<br/><br/>This is a room for unconferencing! The other unconferencing rooms are [[Minetown->minetown]], [[Oracle->oracle]], [[Castle->castle]], [[Sokoban->sokoban]], and [[Astral Plane->astralPlane]]. Or you can go back down to the [[unconferencing lobby->unconference]].',
+export const naga = {
+  id: 'naga',
+  displayName: 'Unconferencing: naga',
+  shortName: 'naga',
+  description: 'This is an unconferencing room. Careful not to get constricted!<br/><br/>This is a room for unconferencing! The other unconferencing rooms are [[cockatrice]], [[dragon]], [[skeleton]], [[tengu]], and [[yak]]. Or you can climb the stairs back up to the [[unconferencing lobby->unconference]].',
   hasNoteWall: true
 }
 
-export const castle = {
-  id: 'castle',
-  displayName: 'Unconferencing: Castle',
-  shortName: 'castle',
-  description: 'A foreboding castle stands before you, surrounded by a moat that ripples with unseen shapes swimming beneath the surface. The front of the castle is taken up by a large drawbridge that is currently raised, preventing you from venturing inside. Strangely, a pile of discarded musical instruments is piled on shore by where the bridge would presumably open - and the area directly beneath the bridge\'s landing zone is smeared with what you hope is ketchup.<br/><br/>We encourage you to use this space to talk "face-to-face" using <a href="https://us02web.zoom.us/j/83231990219?pwd=RGo5cEdveXIyNzJaS0xxdXJsZjRNQT09" target="_blank">the Zoom meeting for this room</a>, which you can access at any time during the conference. (Passcode 390551)<br/><br/>This is a room for unconferencing! The other unconferencing rooms are [[Minetown->minetown]], [[Oracle->oracle]], [[Vlad\'s Tower->tower]], [[Sokoban->sokoban]], and [[Astral Plane->astralPlane]]. Or you can go back to the [[unconferencing lobby->unconference]].',
+export const skeleton = {
+  id: 'skeleton',
+  displayName: 'Unconferencing: skeleton',
+  shortName: 'skeleton',
+  description: 'This is an unconferencing room. bones bones bones<br/><br/>This is a room for unconferencing! The other unconferencing rooms are [[cockatrice]], [[dragon]], [[naga]], [[tengu]], and [[yak]]. Or you can climb the stairs back up to the [[unconferencing lobby->unconference]].',
   hasNoteWall: true
 }
 
-export const sokoban = {
-  id: 'sokoban',
-  displayName: 'Unconferencing: Sokoban',
-  shortName: 'sokoban',
-  description: 'This strange room is made up of blue walls with large circular pits dotting the floor, accompanied by many boulders of matching size. The boulders are strangely easy to roll from place to place - and even stranger is how often moving a boulder reveals a flattened snack underneath. Something about being in this room fills you with a subtle feeling of frustration.<br/><br/>We encourage you to use this space to talk "face-to-face" using <a href="https://us02web.zoom.us/j/88300705663?pwd=ZTZ6N2x0Sks4a21qTTlSREt5NGJhQT09" target="_blank">the Zoom meeting for this room</a>, which you can access at any time during the conference. (Passcode 418594)<br/><br/>This is a room for unconferencing! The other unconferencing rooms are [[Minetown->minetown]], [[Oracle->oracle]], [[Castle->castle]], [[Vlad\'s Tower->tower]], and [[Astral Plane->astralPlane]]. Or you can go back to the [[unconferencing lobby->unconference]].',
+export const tengu = {
+  id: 'tengu',
+  displayName: 'Unconferencing: tengu',
+  shortName: 'tengu',
+  description: 'This is an unconferencing room. Caw caw!<br/><br/>This is a room for unconferencing! The other unconferencing rooms are [[cockatrice]], [[dragon]], [[naga]], [[skeleton]], and [[yak]]. Or you can climb the stairs back up to the [[unconferencing lobby->unconference]].',
   hasNoteWall: true
 }
 
-export const astralPlane = {
-  id: 'astralPlane',
-  displayName: 'Unconferencing: Astral Plane',
-  shortName: 'astral plane',
-  description: 'Your steps feel lighter in this glowing realm among the clouds. The majestic rays of golden light and pure sweet air would make it almost heavenly - if not for the awkward company of several strange and intimidating characters further inside by the three great temples. The way they keep shooting you suspicious glances and murmering to themselves gives you the distinct feeling they don\'t think you should be here.<br/><br/>We encourage you to use this space to talk "face-to-face" using <a href="https://us02web.zoom.us/j/85608238055?pwd=MXIvWHQ2OS85MlFFeHVucHQ1R3g1QT09" target="_blank">the Zoom meeting for this room</a>, which you can access at any time during the conference. (Passcode 753544)<br/><br/>This is a room for unconferencing! The other unconferencing rooms are [[Minetown->minetown]], [[Oracle->oracle]], [[Castle->castle]], [[Vlad\'s Tower->tower]], and [[Sokoban->sokoban]]. Or you can float back down to the [[unconferencing lobby->unconference]].',
+
+export const yak = {
+  id: 'yak',
+  displayName: 'Unconferencing: yak',
+  shortName: 'yak',
+  description: 'This is an unconferencing room. Please, no shaving!<br/><br/>This is a room for unconferencing! The other unconferencing rooms are [[cockatrice]], [[dragon]], [[naga]], [[skeleton]], and [[tengu]]. Or you can climb the stairs back up to the [[unconferencing lobby->unconference]].',
   hasNoteWall: true
 }
