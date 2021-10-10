a34b2a5c151fba8d423279a4a1ea2cc12ef773d0
Kawa <62489292+kawa-kitsuragi@users.noreply.github.com>
2021-10-10 01:34:43 -0400

    Update closedSigns.ts
    you can blame/credit scores on the Grind Rail for Bob E. Cagayan for some of these crimes

--- server/src/generators/closedSigns.ts
+++ server/src/generators/closedSigns.ts
@@ -7,26 +7,86 @@ export const actionString = (poster: string) => {
 export const generate = () => {
   var grammar = tracery.createGrammar({
     origin: [
-      'it reads "#apology# - #closedReason#" in a #writingMethod#'
+      'it reads "#apology# - #closedReason#" in a #writingMethod#',
+      'but all it says is "#closedReason#"',
+      'it\'s #cramped#, but you can make out #closedReason#'
     ],
     apology: [
       'Sorry',
       'Apologies to our guests',
-      "This one's my bad",
-      'Oops'
+      'This one\'s my bad',
+      'Oops',
+      'We apologize for any inconvenience'
     ],
     closedReason: [
       'overrun with crabs',
-      '400 destination not found',
+      '#n# destination not found',
       'we lost this one',
-      'dog ate our airport'
+      'dog ate our airport',
+      'we could\'nt figure out the code to lower the drawbridge',
+      'we require #n# #runes#',
+      'a cat is sitting on me and won\'t leave',
+      'Xom finds this hilarious!'
     ],
     writingMethod: [
       'hasty handwritten scrawl',
       'messy crayon scribble',
       'comic sans font',
       'standard printed font',
-      'series of letters cut from magazines'
+      'series of letters cut from magazines',
+      'unfairly beautiful calligraphy'
+    ],
+    cramped: [
+      'full of legalese',
+      'scribbled over in crayon',
+      'full of ever-shifting ASCII'
+    ],
+    runes: [
+      'runes',
+      'MacGuffins',
+      'lumenstones',
+      'salmon',
+      'spikes',
+      'plump helmets',
+      'sharks',
+      'excuses'
+    ],
+    n: [
+      '#thousands##digit##digit##digit#',
+      '#leading##digit##digit#',
+      '#leading##digit#',
+      '#leading#',
+      '#leading#'
+    ],
+    thousands:[
+      '1',
+      '2',
+      '3',
+      '4',
+      '5'
+    ],
+    leading: [
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9'
+    ],
+    digit: [
+      '0',
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9'
     ]
   })
 
