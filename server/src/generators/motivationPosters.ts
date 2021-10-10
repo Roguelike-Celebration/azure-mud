02e3fa7650fa7101f53a0ab6fc5aee0d09ec94b8
Kawa <62489292+kawa-kitsuragi@users.noreply.github.com>
2021-10-09 22:25:57 -0400

    more generator fun

--- server/src/generators/motivationPosters.ts
+++ server/src/generators/motivationPosters.ts
@@ -1,21 +1,40 @@
 var tracery = require('tracery-grammar')
 
 export const actionString = (item: string) => {
-  return `You look more closely at one of the many posters - ${item}.`
+  return `You look more closely at one of the many posters. ${item}.`
 }
 
 export const generate = () => {
   var grammar = tracery.createGrammar({
     origin: [
-      'it says "#slogan#" in bold face type.',
-      'Someone has scrawled "#slogan#" over it, large enough you cannot see the original text.'
+      'It says "#slogan#" in bold face type.',
+      'Someone has scrawled "#slogan#" over it, large enough you cannot see the original text.',
+      `It's filled with all sorts of text, but you can make out "#slogan#"`,
+      '#afriendly# skeleton is saying "#slogan#'
     ],
     slogan: [
       'bones bones bones',
       'The Enemy Within',
       'The Power Inside You - Bones',
       'Hit da bricks! Real winners quit!',
-      'Did you know you have a skeleton inside you right now?'
+      'Did you know you have a skeleton inside you right now?',
+      'Drink more milk! Calcium is good for you!',
+      'Flesh is overrated.',
+      'I can see right through you!'
+    ],
+    aFriendly: [
+      'A #friendly#',
+      'An #appealing#'
+    ],
+    friendly: [
+      'friendly',
+      'delightful',
+      'boisterous'
+    ],
+    appealing: [
+      'appealing',
+      'excited',
+      'oddly attractive'
     ]
   })
 
