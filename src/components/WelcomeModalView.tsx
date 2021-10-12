4a8c9c6d3a933dcf2d6f77e1c4b50361f1c80aac
Kawa <62489292+kawa-kitsuragi@users.noreply.github.com>
2021-10-11 23:01:29 -0400

    Update WelcomeModalView.tsx
    A note about audio/video, happy to edit if need be

--- src/components/WelcomeModalView.tsx
+++ src/components/WelcomeModalView.tsx
@@ -7,6 +7,7 @@ export default function WelcomeModalView (props: {}) {
       <p>In the Roguelike Celebration conference space,
         you can only send/receive messages <strong>in the chat room you are currently in</strong>. </p>
       <p>You can move from virtual room to virtual room to talk to different people.</p>
+      <p>Most rooms have integrated audio and video chat. You cannot be seen or heard unless you explicitly opt-in!</p>
       <p>Our hope is to facilitate smaller group conversations, to make Roguelike Celebration feel a little more like a real-world conference, and to give you a fun and playful space to explore with your fellow attendees.</p>
       <p>Happy wandering, and be wary of reading scrolls without identifying them first ;)</p>
       <p>-The Roguelike Celebration team</p>
