160a0c7632d9728bf21f09314fcd8ce35796419e
Travis Moy <moytravis@gmail.com>
2021-10-11 17:40:06 -0700

    Add breakout rooms to Happening Now
    Also update scheduley

--- src/components/HappeningNowView.tsx
+++ src/components/HappeningNowView.tsx
@@ -14,9 +14,10 @@ export default function HappeningNowView (props: { roomData: { [roomId: string]:
   const dispatch = useContext(DispatchContext)
 
   // To test this functionality, just set it at some appropriate date in the conference time
-  // const now = new Date(`2020-10-03T18:45:00.000-07:00`)
+  // const now = new Date(`2021-10-16T10:35:00.000-07:00`)
   const now = new Date()
-  const currentlyScheduled = ReversedScheduleEntries.find((entry) => entry.time < now)
+  const currentlyScheduledIdx = ReversedScheduleEntries.findIndex((entry) => entry.time < now)
+  const currentlyScheduled = ReversedScheduleEntries[currentlyScheduledIdx]
 
   const moveAndClose = (roomId: string) => {
     if (!roomData[roomId]) {
@@ -41,10 +42,39 @@ export default function HappeningNowView (props: { roomData: { [roomId: string]:
 
   let currentlyScheduledElement
   if (currentlyScheduled) {
-    currentlyScheduledElement = <div id ='currently-scheduled-div'>
-      {currentlyScheduled.text}
-      {currentlyScheduled.roomIds ? buildScheduledRoomList(currentlyScheduled.roomIds) : ''}
-    </div>
+    if (currentlyScheduled.text == 'Social Time') {
+      var i = currentlyScheduledIdx + 1;
+      const socialTimeOptions = []
+      while (ReversedScheduleEntries[i] && ReversedScheduleEntries[i].breakoutRoomId) {
+        const breakoutEntry = ReversedScheduleEntries[i]
+        const breakoutRoomId = breakoutEntry.breakoutRoomId
+        const discussString = `Discuss ${breakoutEntry.text} in `
+        socialTimeOptions.push(
+          <li key={breakoutRoomId}>
+            {discussString}
+            <button id={`button-happening-now${breakoutRoomId}`} onClick={() => moveAndClose(breakoutRoomId)} className='link-styled-button'>
+              {props.roomData[breakoutRoomId] ? props.roomData[breakoutRoomId].name : 'unknown room'}
+            </button>
+          </li>
+          )
+        i++
+      }
+      socialTimeOptions.push(<li key={'option-chat'}>Chat!</li>)
+      socialTimeOptions.push(<li key={'option-explore'}>Explore the space!</li>)
+      socialTimeOptions.push(<li key={'option-hunt'}>Hunt for secrets!</li>)
+      socialTimeOptions.push(<li key={'option-free'}>Just remember to beware the grues.</li>)
+      currentlyScheduledElement = <div id = 'currently-scheduled-div'>
+        <strong>Social Time</strong>
+        <ul>
+          {socialTimeOptions}
+        </ul>
+      </div>
+    } else {
+      currentlyScheduledElement = <div id ='currently-scheduled-div'>
+        {currentlyScheduled.text}
+        {currentlyScheduled.roomIds ? buildScheduledRoomList(currentlyScheduled.roomIds) : ''}
+      </div>
+    }
   } else {
     currentlyScheduledElement = <strong>You&apos;re in early! Check the schedule for when the doors officially open.</strong>
   }
