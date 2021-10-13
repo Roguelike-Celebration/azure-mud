d6c5b9fc36a7d40a350bb6964ace85abf3e5c9b8
Em Lazer-Walker <me@lazerwalker.com>
2021-10-13 11:57:02 -0400

    Tweak welcome message [Closes #476]

--- src/components/WelcomeModalView.tsx
+++ src/components/WelcomeModalView.tsx
@@ -4,12 +4,26 @@ export default function WelcomeModalView (props: {}) {
   return (
     <div>
       <h1>This isn&apos;t a normal chat app!</h1>
-      <p>In the Roguelike Celebration conference space,
-        you can only send/receive messages <strong>in the chat room you are currently in</strong>. </p>
-      <p>You can move from virtual room to virtual room to talk to different people.</p>
-      <p>Most rooms have integrated audio and video chat. You cannot be seen or heard unless you explicitly opt-in!</p>
-      <p>Our hope is to facilitate smaller group conversations, to make Roguelike Celebration feel a little more like a real-world conference, and to give you a fun and playful space to explore with your fellow attendees.</p>
-      <p>Happy wandering, and be wary of reading scrolls without identifying them first ;)</p>
+      <p>
+        In the Roguelike Celebration conference space, you can only send/receive
+        messages <strong>in the chat room you are currently in</strong>.
+      </p>
+      <p>
+        You can move from virtual room to virtual room to talk to different
+        people.
+      </p>
+      <p>
+        You may see or hear others on videochat, but they can&apos;t see or hear you
+        unless you explicitly turn on your audio or video.
+      </p>
+      <p>
+        Our hope is to facilitate smaller group conversations, to capture the feel of an in-person conference, and to give
+        you a fun and playful space to explore with your fellow attendees.
+      </p>
+      <p>
+        Happy wandering, and be wary of reading scrolls without identifying them
+        first ;)
+      </p>
       <p>-The Roguelike Celebration team</p>
     </div>
   )
