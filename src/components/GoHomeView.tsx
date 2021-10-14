52bc0af1961c1dac315c1046bd534894fb0e9ac0
Travis Moy <moytravis@gmail.com>
2021-10-14 14:42:28 -0700

    Update keep out link for 2021

--- src/components/GoHomeView.tsx
+++ src/components/GoHomeView.tsx
@@ -4,7 +4,7 @@ export default function GoHomeView () {
   return (
     <div id='go-home'>
       <h1>A magical force repels you from the entrance</h1>
-      <p>Roguelike Celebration is currently closed! Check the <a href="https://roguelike.club/event2020.html" target="_blank" rel="noreferrer">schedule</a> to see when our doors will be open again.</p>
+      <p>Roguelike Celebration is currently closed! Check the <a href="https://roguelike.club/event2021.html" target="_blank" rel="noreferrer">schedule</a> to see when our doors will be open again.</p>
       <p>We look forward to celebrating again with you soon :)</p>
     </div>
   )
