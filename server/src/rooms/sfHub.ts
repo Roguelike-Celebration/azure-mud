51460e934ab9399cd07ef39934f72558e4a2f315
Kawa <62489292+kawa-kitsuragi@users.noreply.github.com>
2021-10-14 18:41:11 -0600

    extra social space at bar, robot lab quiet space

--- server/src/rooms/sfHub.ts
+++ server/src/rooms/sfHub.ts
@@ -2,7 +2,7 @@ export const sfHub = {
   id: 'sfHub',
   displayName: 'Space Hanger Observation Lounge',
   shortName: 'Space Lounge',
-  description: 'An impossibly fast elevator whisks you up to a bright lounge of steel, chrome, and futuristic plastics. Spacecraft of all shapes and sizes constantly come and go in view of the giant observation window. Constrasting the vast depths of space, the lounge itself is strangely cozy. Floor-to-ceiling hexagonal bookshelves provide all the reading material you could ask for, with oddly shaped but fluffy ottoman-like chairs to settle into. A [[vending machine->vendingMachine]] offers refreshment, along with an extensive bar serving Cosmic Cryo-Brewed Coffee, Terrific Techno-Tea, and apple juice.</br></br>A little robot beckons you to the [[Robot Fabrication Lab->robots]]. In the corner, a [[dusty machine->timeMachine]] blinks invitingly. You can also go back to the central [[hall]].',
+  description: 'An impossibly fast elevator whisks you up to a bright lounge of steel, chrome, and futuristic plastics. Spacecraft of all shapes and sizes constantly come and go in view of the giant observation window. Constrasting the vast depths of space, the lounge itself is strangely cozy. Floor-to-ceiling hexagonal bookshelves provide all the reading material you could ask for, with oddly shaped but fluffy ottoman-like chairs to settle into. A [[vending machine->vendingMachine]] offers refreshment, along with an extensive bar serving Cosmic Cryo-Brewed Coffee, Terrific Techno-Tea, and apple juice.</br></br>A little robot, sensing you might need a place to relax quietly, beckons you to the [[Robot Fabrication Lab->robots]]. In the corner, a [[dusty machine->timeMachine]] blinks invitingly. You can also go back to the central [[hall]].',
   hasNoteWall: true,
   noteWallData: {
     roomWallDescription: 'Among the seemingly infinite bookshelves, one has been emptied out and contains only a small pile of notebooks. A sign next to it reads "BOOKMARKS - PLEASE HELP US SIFT THE GOOD STUFF!"',
@@ -17,7 +17,8 @@ export const robots = {
   id: 'robots',
   displayName: 'Robot Fabrication Lab',
   shortName: 'robot lab',
-  description: 'Something witty about making a robot friend. Go back to the [[Spacehanger->sfHub]].'
+  noMediaChat: true,
+  description: 'This room is dominated by the quiet hum of machinery doing what it was designed to with minimal fuss and expertly crafted efficiency. Sprockets are being socketed in in one device. A robot arm picks up the socketed sprocket and passes it to a CNC spot welder, which welds it closed. The locked socketed sprockets are conveyed into an opening on the biggest machine, just one of many components for the robots produced at this fab lab. The entire dance is carried out with a ballet-like elegance, not a movement wasted. It\'s aggressively, profoundly satisfying. When you feel ready, you can go back to the [[Spacehanger->sfHub]].'
 }
 
 export const timeMachine = {
