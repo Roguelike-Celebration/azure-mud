0f05c90a7e581c3f17a8348955dc84b370d24251
Matt Hayes <matt@mysterycommand.com>
2022-08-27 22:31:38 -0500

    style: format document (forced)

--- src/components/ChatView.tsx
+++ src/components/ChatView.tsx
@@ -4,41 +4,63 @@ import { FixedSizeList } from 'react-window'
 import AutoSizer from 'react-virtualized-auto-sizer'
 
 import MessageView from './MessageView'
-import { Message, MessageType, ConnectedMessage, DisconnectedMessage, EnteredMessage, LeftMessage } from '../message'
+import {
+  Message,
+  MessageType,
+  ConnectedMessage,
+  DisconnectedMessage,
+  EnteredMessage,
+  LeftMessage
+} from '../message'
 
 import '../../style/chat.css'
 import { DispatchContext } from '../App'
-import { ActivateAutoscrollAction, DeactivateAutoscrollAction } from '../Actions'
+import {
+  ActivateAutoscrollAction,
+  DeactivateAutoscrollAction
+} from '../Actions'
 import { ServerSettings } from '../../server/src/types'
 
-function isMovementMessage (message: Message): message is ConnectedMessage | DisconnectedMessage | EnteredMessage | LeftMessage {
-  return message.type === MessageType.Connected || message.type === MessageType.Disconnected ||
-    message.type === MessageType.Entered || message.type === MessageType.Left
+function isMovementMessage (
+  message: Message
+): message is
+  | ConnectedMessage
+  | DisconnectedMessage
+  | EnteredMessage
+  | LeftMessage {
+  return (
+    message.type === MessageType.Connected ||
+    message.type === MessageType.Disconnected ||
+    message.type === MessageType.Entered ||
+    message.type === MessageType.Left
+  )
 }
 
 interface Props {
-  messages: Message[],
-  autoscrollChat: boolean,
-  serverSettings: ServerSettings,
-  captionsEnabled: boolean
+  messages: Message[];
+  autoscrollChat: boolean;
+  serverSettings: ServerSettings;
+  captionsEnabled: boolean;
 }
 
 interface RowProps {
-  data: Message,
-  index: number,
-  isScrolling: boolean,
-  style: any,
+  data: Message;
+  index: number;
+  isScrolling: boolean;
+  style: any;
 }
 
 // eslint-disable-next-line react/prop-types
-const Row: FC<RowProps> = ({ data, index, style }) => (<MessageView
-  message={data[index]}
-  key={`message-${index}`}
-  id={`message-${index}`}
-  hideTimestamp={false}
-  msgIndex={index}
-  style={style}
-/>)
+const Row: FC<RowProps> = ({ data, index, style }) => (
+  <MessageView
+    message={data[index]}
+    key={`message-${index}`}
+    id={`message-${index}`}
+    hideTimestamp={false}
+    msgIndex={index}
+    style={style}
+  />
+)
 
 export default function ChatView (props: Props) {
   const dispatch = useContext(DispatchContext)
@@ -56,36 +78,42 @@ export default function ChatView (props: Props) {
 
   const outerRef = useRef<HTMLDivElement | null>(null)
   const listRef = useRef<FixedSizeList | null>(null)
-  const handleScroll = useCallback(({
-    scrollDirection,
-    scrollOffset,
-    scrollUpdateWasRequested
-  }: {
-    scrollDirection: 'forward' | 'backward',
-    scrollOffset: number,
-    scrollUpdateWasRequested: boolean
-  }) => {
-    if (outerRef.current === null) {
-      return
-    }
+  const handleScroll = useCallback(
+    ({
+      scrollDirection,
+      scrollOffset,
+      scrollUpdateWasRequested
+    }: {
+      scrollDirection: 'forward' | 'backward';
+      scrollOffset: number;
+      scrollUpdateWasRequested: boolean;
+    }) => {
+      if (outerRef.current === null) {
+        return
+      }
 
-    const isScrolledToBottom = outerRef.current.scrollHeight === outerRef.current.scrollTop + outerRef.current.clientHeight
-    if (isScrolledToBottom && !props.autoscrollChat) {
-      dispatch(ActivateAutoscrollAction())
-    } else if (!isScrolledToBottom && props.autoscrollChat) {
-      dispatch(DeactivateAutoscrollAction())
-    }
-  }, [props.autoscrollChat, outerRef.current])
+      const isScrolledToBottom =
+        outerRef.current.scrollHeight ===
+        outerRef.current.scrollTop + outerRef.current.clientHeight
+      if (isScrolledToBottom && !props.autoscrollChat) {
+        dispatch(ActivateAutoscrollAction())
+      } else if (!isScrolledToBottom && props.autoscrollChat) {
+        dispatch(DeactivateAutoscrollAction())
+      }
+    },
+    [props.autoscrollChat, outerRef.current]
+  )
 
-  const [shouldShowOlderMessages, setShouldShowOlderMessages] = React.useState(false)
+  const [shouldShowOlderMessages, setShouldShowOlderMessages] =
+    React.useState(false)
 
   // This message filtering logic is kinda ugly and hard to read
   function shouldRemoveMessage (m: Message) {
-    return isMovementMessage(m) &&
-      (
-        props.serverSettings.movementMessagesHideRoomIds.includes(m.roomId) ||
-        m.numUsersInRoom > props.serverSettings.movementMessagesHideThreshold
-      )
+    return (
+      isMovementMessage(m) &&
+      (props.serverSettings.movementMessagesHideRoomIds.includes(m.roomId) ||
+        m.numUsersInRoom > props.serverSettings.movementMessagesHideThreshold)
+    )
   }
   const messages = props.messages
     .filter((msg) => {
@@ -100,10 +128,12 @@ export default function ChatView (props: Props) {
 
   const lastIndexOfMovedMessage = findLastIndex(
     messages,
-    message => message.type === MessageType.MovedRoom
+    (message) => message.type === MessageType.MovedRoom
   )
   const currentRoomMessages = messages.slice(lastIndexOfMovedMessage)
-  const shownMessages = shouldShowOlderMessages ? messages : currentRoomMessages
+  const shownMessages = shouldShowOlderMessages
+    ? messages
+    : currentRoomMessages
 
   useEffect(() => {
     // const lastMessage = document.querySelector(
@@ -150,7 +180,8 @@ export default function ChatView (props: Props) {
               itemSize={24}
             >
               {Row}
-            </FixedSizeList>)}
+            </FixedSizeList>
+          )}
         </AutoSizer>
       </div>
       {/* <div id="messages" onScroll={handleScroll}>
