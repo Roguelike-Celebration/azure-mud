2f6447ef674d861acb7a72377a073bc858a22e30
Matt Hayes <matt@mysterycommand.com>
2022-09-04 11:26:55 -0500

    refactor: remove react-window and react-virtualize-auto-sizer, remove old imp'l

--- src/components/ChatView.tsx
+++ src/components/ChatView.tsx
@@ -1,14 +1,5 @@
 import { findLastIndex } from 'lodash'
-import React, {
-  FC,
-  useCallback,
-  useContext,
-  useEffect,
-  useRef,
-  useState
-} from 'react'
-import AutoSizer from 'react-virtualized-auto-sizer'
-import { FixedSizeList } from 'react-window'
+import React, { useState } from 'react'
 
 import {
   ConnectedMessage,
@@ -18,15 +9,9 @@ import {
   Message,
   MessageType
 } from '../message'
-import MessageView from './MessageView'
 
 import { ServerSettings } from '../../server/src/types'
 import '../../style/chat.css'
-import {
-  ActivateAutoscrollAction,
-  DeactivateAutoscrollAction
-} from '../Actions'
-import { DispatchContext } from '../App'
 
 type MovementMessage =
   | ConnectedMessage
@@ -51,56 +36,7 @@ interface Props {
   captionsEnabled: boolean;
 }
 
-interface RowProps {
-  data: Message;
-  index: number;
-  isScrolling: boolean;
-  style: any;
-}
-
-// eslint-disable-next-line react/prop-types
-const Row: FC<RowProps> = ({ data, index, style }) => (
-  <MessageView
-    message={data[index]}
-    key={`message-${index}`}
-    id={`message-${index}`}
-    hideTimestamp={false}
-    msgIndex={index}
-    style={style}
-  />
-)
-
 export default function ChatView (props: Props) {
-  const dispatch = useContext(DispatchContext)
-
-  const outerRef = useRef<HTMLDivElement | null>(null)
-  const listRef = useRef<FixedSizeList | null>(null)
-  const handleScroll = useCallback(
-    ({
-      scrollDirection,
-      scrollOffset,
-      scrollUpdateWasRequested
-    }: {
-      scrollDirection: 'forward' | 'backward';
-      scrollOffset: number;
-      scrollUpdateWasRequested: boolean;
-    }) => {
-      if (outerRef.current === null) {
-        return
-      }
-
-      const isScrolledToBottom =
-        outerRef.current.scrollHeight ===
-        outerRef.current.scrollTop + outerRef.current.clientHeight
-      if (isScrolledToBottom && !props.autoscrollChat) {
-        dispatch(ActivateAutoscrollAction())
-      } else if (!isScrolledToBottom && props.autoscrollChat) {
-        dispatch(DeactivateAutoscrollAction())
-      }
-    },
-    [props.autoscrollChat, outerRef.current]
-  )
-
   const [shouldShowOlderMessages, setShouldShowOlderMessages] = useState(false)
 
   // This message filtering logic is kinda ugly and hard to read
@@ -111,6 +47,7 @@ export default function ChatView (props: Props) {
         m.numUsersInRoom > props.serverSettings.movementMessagesHideThreshold)
     )
   }
+
   const messages = props.messages
     .filter((msg) => {
       // Hide movement messages if the room is busy enough
@@ -126,20 +63,11 @@ export default function ChatView (props: Props) {
     messages,
     (message) => message.type === MessageType.MovedRoom
   )
+
   const currentRoomMessages = messages.slice(lastIndexOfMovedMessage)
   const shownMessages = shouldShowOlderMessages
     ? messages
     : currentRoomMessages
-
-  useEffect(() => {
-    if (listRef.current === null) {
-      return
-    }
-
-    if (props.autoscrollChat) {
-      listRef.current.scrollToItem(shownMessages.length - 1, 'end')
-    }
-  }, [shownMessages.length, props.autoscrollChat, listRef.current])
 
   return (
     <>
@@ -150,23 +78,6 @@ export default function ChatView (props: Props) {
         {shouldShowOlderMessages ? 'Hide' : 'Show'} Older Messages
       </button>
       <div id="messages">
-        <AutoSizer>
-          {({ width, height }) => (
-            <FixedSizeList
-              ref={listRef}
-              outerRef={outerRef}
-              onScroll={handleScroll}
-              width={width}
-              height={height}
-              initialScrollOffset={height}
-              itemData={shownMessages}
-              itemCount={shownMessages.length}
-              itemSize={24}
-            >
-              {Row}
-            </FixedSizeList>
-          )}
-        </AutoSizer>
       </div>
     </>
   )
