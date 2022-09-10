e8cdef546d917790d027553d9f06e7b4e458efaf
TheBald <nathanielsavant@gmail.com>
2022-09-10 14:58:38 -0700

    updating roomData and the rainbow/dull door files such that the recoloring mechanic is present in the experimental biology lab, run by Dr Hope. Shortened the descriptive text for these features, and simplified the experience a little as well. Hopefully all of this actually works!

--- src/components/feature/DullDoorViews.tsx
+++ src/components/feature/DullDoorViews.tsx
@@ -22,7 +22,7 @@ export const DullDoorRoomView = () => {
   }
 
   return <div id="dull-door-div" className="feature-room-view">
-    <p>Inside the glass tube there&apos;s nothing in particular, though you note that there&apos;s a grate on the floor. A damp sign next to the tube&apos;s opening says &quot;Secondarily-Hydrogenated Oxygen Weighted Emulsion Remover&quot;. A smaller sign below that reads &quot;For Dirts, Dyes, and Distress!&quot; <button id="dull-door-button" className='link-styled-button' onClick={walkThroughDoor}>Step on the grate</button>.</p>
+    <p><button id="dull-door-button" className='link-styled-button' onClick={walkThroughDoor}>Pull the clean rope.</button>.</p>
   </div>
 }
 
@@ -49,17 +49,13 @@ export default function DullDoorModalView () {
     return (
       <div>
         <h1>Refreshing!</h1>
-        <p>As you step into the tube, the grating beneath you depresses and clicks. Suddenly the machine gurgles and from openings the top of the tube, warm Secondarily-Hydrogenated Oxygen (miracle of the modern age!) streams down on you and rinses any Dirts, Dyes, or Distress you may have accrued away. It&apos;s soothing, in its own odd way, but you aren&apos;t sure this so-called S.H.O.W.E.R. will catch on with the masses.</p>
+        <p>Doctor Hope gives you a bored look of disappointment as a stream of clear liquid falls down from the open pipes above you. The liquid covers your body, leaving you feeling refreshed.</p>
       </div>
     )
   } else {
     return (
       <div>
-        <h1>Could be refreshing?</h1>
-        <p>You tentatively press a toe onto the grate. Suddenly the machine gurgles and from openings the top of the tube, warm Secondarily-Hydrogenated Oxygen (miracle of the modern age!) streams down! It certainly seems like any Dirts, Dyes, or Distress you may have accrued will be washed away if you step in fully. It&apos;ll probably be refreshing, but it will <em>definitely</em> take the paint from the doctor(?) with it.</p>
-        <p><strong>Fully enter the S.H.O.W.E.R.?</strong></p>
-        <button id='dull-door-agree' onClick={giveColor}>Enter</button>
-        <button id='dull-door-agree' onClick={leave}>Leave</button>
+        <p>As you sit in the chair, Doctor Hope looks up with excitement and begins to approach. As your hand touches the clean rope, however, she rolls her eyes and returns to her work. A stream of clear liquid covers your body, leaving you feeling refreshed.</p>
       </div>
     )
   }
--- src/components/feature/RainbowGateViews.tsx
+++ src/components/feature/RainbowGateViews.tsx
@@ -22,7 +22,7 @@ export const RainbowGateRoomView = () => {
   }
 
   return <div id="rainbow-gate-div" className="feature-room-view">
-    <p>Do you, in fact, approach the chair? <button id="rainbow-gate-button" className='link-styled-button' onClick={jumpThroughGate}>Approach the chair.</button></p>
+    <p>Sit and pull a rope? <button id="rainbow-gate-button" className='link-styled-button' onClick={jumpThroughGate}>Pull the paint-spattered rope.</button></p>
   </div>
 }
 
@@ -40,19 +40,16 @@ export default function RainbowGateModalView () {
   if (visits === 1) {
     return (
       <div>
-        <h1>It&apos;s a miracle of modern science!</h1>
-        <p>&quot;Oho! Why, you&apos;ve arrived just in time for the most amazing experience you&apos;ll ever... experience! Just sit in this chair, pull that lever, and you&apos;ll in for a truly eye-opening journey. Why, it&apos;s a miracle of modern science!&quot;</p>
-        <p>You sit in the chair (it&apos;s not especially comfortable and it&apos;s... slightly wet?) and pull the lever. There&apos;s a shudder as the machine rumbles to life and things start happening. Gears spin, lightning arcs up two metal trusses you hadn&apos;t noticed. Your hair begins to stand on end. The doctor (you didn&apos;t see any diplomas, so who&apos;s to say?) starts to cackle wildly, yelling, &quot;It works! It WORKS!&quot; to no one in particular. The machine&apos;s hum hits a crescendo when suddenly everything comes to a stop. All is silence. You wait for a moment when you hear a buzz above you.</p>
-        <p>Looking up, you see a mechanical arm holding a bucket extend out and rotate, spilling a bucket of paint all over you. &quot;Yes!&quot; yells the doctor (?) &quot;They said it couldn&apos;t be done! Now... if you&apos;ll excuse me, I need to clean the paint off the chair.&quot; They hold up a sponge and bucket of water and gesture at you to make yourself scarce.</p>
+        <p>Doctor Hope looms over you with a smile. &quot;You wish to help in my experiments? Just sit in this chair, pull that rope, and you&apos;ll in for a truly eye-opening journey. Why, it&apos;s a miracle of modern science!&quot;</p>
+        <p>You sit in the chair (it&apos;s not especially comfortable and it&apos;s ... slightly wet?) and pull the rope. A roar of machinery echoes through the air as gears spin, and lightning arcs up two metal trusses you hadn&apos;t noticed. The doctor starts to cackle wildly, yelling, &quot;It works! It WORKS!&quot; to no one in particular. The machine&apos;s hum hits a crescendo when suddenly everything comes to a stop. Silence. A bucket of paint falls unceremoniously from the ceiling, landing on you.</p>
         <p><em>Your chat name color is now <span className={`name ${userMap[myId].nameColor}`}>{userMap[myId].nameColor}</span></em>.</p>
       </div>
     )
   } else {
     return (
       <div>
-        <h1>Back for more?</h1>
-        <p>The ambiguously credentialed operator gives you a nod. &quot;Back for more, huh? Er, wait, no. I mean... *ahem* So! You&apos;ve returned for another glimpse at the uncanny realities beyond human perception! Well sit right down and pull yon lever, my friend.&quot; They floridly gesture to the chair at the center of the paint-dumping apparatus.</p>
-        <p>You pull the lever and nothing appears to happen. Then the machine operator runs up with a bucket of paint and spills it all over you. &quot;So sorry, we only have the aetheric budget to do the big show once per person. Grant writing, am I right?&quot; They look you over. &quot;You know, you don&apos;t look that different to me, but what do I know?&quot; They tap the multi-lensed googles obscuring most of their face. &quot;With these things on it&apos;s a wonder I can see at all. Everything is coming through like an original Game Boy screen.&quot;</p>
+        <p>The ambiguously credentialed operator gives you a nod. &quot;Back for more, huh? I appreciate a fellow lover of science!&quot;</p>
+        <p>You sit in the damp, uncomfortable chair, and pull the rope. You have just enough time to notice that the rope was attached directly to the bucket this time around. It lands on you with a loud squelching noise.</p>
         <p><em>Your chat name color is now <span className={`name ${userMap[myId].nameColor}`}>{userMap[myId].nameColor}</span></em>.</p>
       </div>
     )
