01ed001d7dbbf47ea0b6c70f0630f5cd91b06158
Matt Hayes <matt@mysterycommand.com>
2022-10-09 16:30:56 -0500

    feat: add hr for moved room message, render nothing if message is undefined

--- src/components/MessageItem/MessageItem.tsx
+++ src/components/MessageItem/MessageItem.tsx
@@ -1,5 +1,6 @@
 import React, { FC, memo, useContext } from 'react'
 import { MessagesContext } from '../../App'
+import { MessageType } from '../../message'
 import MessageView from '../MessageView'
 import './MessageItem.css'
 
@@ -12,11 +13,13 @@ interface MessageItemProps {
 export const MessageItem: FC<MessageItemProps> = memo(
   ({ messageId, hideTimestamp, msgIndex }) => {
     const { entities } = useContext(MessagesContext)
+    const message = entities[messageId]
 
-    return (
+    return message && (
       <li className="message-item">
+        {message.type === MessageType.MovedRoom && <hr />}
         <MessageView
-          message={entities[messageId]}
+          message={message}
           hideTimestamp={hideTimestamp}
           msgIndex={msgIndex}
         />
