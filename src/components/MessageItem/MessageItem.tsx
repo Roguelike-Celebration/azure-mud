06c096cbec323d2fb908c448d43d3e06419547c9
Matt Hayes <matt@mysterycommand.com>
2022-10-13 22:12:33 -0500

    refactor: dispatch message render state from MessageItem

--- src/components/MessageItem/MessageItem.tsx
+++ src/components/MessageItem/MessageItem.tsx
@@ -1,7 +1,8 @@
-import React, { FC, memo, useContext } from 'react'
+import React, { FC, memo, useContext, useEffect, useRef } from 'react'
 import { MessagesContext } from '../../App'
 import { MessageType } from '../../message'
 import MessageView from '../MessageView'
+import { VirtualizationContext } from '../VirtualizationProvider/VirtualizationProvider'
 import './MessageItem.css'
 
 interface MessageItemProps {
@@ -10,13 +11,59 @@ interface MessageItemProps {
   msgIndex: number;
 }
 
+const { getComputedStyle: getStyle } = window
+
+const outerTop: (el: HTMLElement) => number = (el) => {
+  const { marginTop } = getStyle(el)
+  return el.offsetTop - parseInt(marginTop, 10)
+}
+
+const outerHeight: (el: HTMLElement) => number = (el) => {
+  const { marginTop, height, marginBottom } = getStyle(el)
+  return [marginTop, height, marginBottom].reduce(
+    (acc, px) => acc + parseInt(px, 10),
+    0
+  )
+}
+
 export const MessageItem: FC<MessageItemProps> = memo(
   ({ messageId, hideTimestamp, msgIndex }) => {
+    const [{ positions, viewportClientHeight }, virtualizationDispatch] =
+      useContext(VirtualizationContext)
     const { entities } = useContext(MessagesContext)
     const message = entities[messageId]
 
+    const messageItemRef = useRef<HTMLLIElement>(null)
+    useEffect(() => {
+      if (!messageItemRef.current) {
+        return
+      }
+
+      const { top, height } = positions[messageId]
+
+      if (top === undefined && height === undefined) {
+        virtualizationDispatch({
+          type: 'setVerticalPosition',
+          payload: {
+            id: messageId,
+            top: viewportClientHeight + outerTop(messageItemRef.current),
+            height: outerHeight(messageItemRef.current)
+          }
+        })
+      }
+    }, [
+      messageId,
+      positions[messageId],
+      viewportClientHeight,
+      virtualizationDispatch
+    ])
+
     return message ? (
-      <li className="message-item">
+      <li
+        className="message-item"
+        style={{ top: positions[messageId]?.top }}
+        ref={messageItemRef}
+      >
         {message.type === MessageType.MovedRoom && <hr />}
         <MessageView
           message={message}
