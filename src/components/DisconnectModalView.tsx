3c3c4b3bef0074c3331f9e5513e540c1d23afe4b
Travis Moy <moytravis@gmail.com>
2022-10-13 20:36:45 -0700

    WHY IS IT WORKING INTERMITTENTLY
    uuuugh

--- src/components/DisconnectModalView.tsx
+++ src/components/DisconnectModalView.tsx
@@ -1,47 +1,75 @@
 import { HubConnection, HubConnectionState } from '@microsoft/signalr'
 import React, { useEffect } from 'react'
-import { HideModalAction } from '../Actions'
+import { HideModalAction, SignalRHubCreatedAction } from '../Actions'
 import { DispatchContext } from '../App'
 import { connectSignalR } from '../networking'
 
 export default function DisconnectModalView (props: { userId: string, connection: HubConnection }) {
+  const [reconnecting, setReconnecting] = React.useState(false)
   const [secondsToReconnect, setsecondsToReconnect] = React.useState(1)
   const [nextSecondsToReconnect, setNextSecondsToReconnect] = React.useState(2)
 
   const dispatch = React.useContext(DispatchContext)
+
+  const forceReconnect = () => {
+    console.log("HARD RECONNECTING!")
+    setReconnecting(true)
+    connectSignalR(props.userId, dispatch).then((newConnection) => {
+      if (
+        newConnection.state === HubConnectionState.Connected ||
+        newConnection.state === HubConnectionState.Connecting ||
+        newConnection.state === HubConnectionState.Reconnecting
+      ) {
+        console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        dispatch(HideModalAction())
+        setReconnecting(false)
+        dispatch(SignalRHubCreatedAction(newConnection))
+      } else {
+        setReconnecting(false)
+        console.log("RECONNECT FAILED")
+        setNextSecondsToReconnect(nextSecondsToReconnect * 2)
+        setsecondsToReconnect(nextSecondsToReconnect)
+      }
+    }).catch(() => {
+      console.log("FAILURE TO CONNECT SHOULD RESET SECONDS")
+      setNextSecondsToReconnect(nextSecondsToReconnect * 2)
+      setsecondsToReconnect(nextSecondsToReconnect)
+      setReconnecting(false)
+    })
+  }
 
   useEffect(() => {
     console.log("in useEffect " + secondsToReconnect)
     // This actually sets & clears the timer every second anew.
     // TODO: better text
     // TODO: what if you actually...time out?
+
     let timerId = setInterval(
       () => {
-        if (secondsToReconnect === 0) {
-          console.log("HARD RECONNECTING!")
-          if (props.connection.state === HubConnectionState.Disconnected) {
-            console.log('PROPS CONN STATE BEFORE ' + props.connection.state)
-            connectSignalR(props.userId, dispatch).then((newConnection) => {
-              if (newConnection.state === HubConnectionState.Connected) {
-                dispatch(HideModalAction())
-                console.log('PROPS CONN STATE AFTER ' + props.connection.state)
-              } else {
-                console.log("RECONNECT FAILED")
-                setNextSecondsToReconnect(nextSecondsToReconnect * 2)
-                setsecondsToReconnect(nextSecondsToReconnect)
-              }
-              
-            }).catch(() => {
-              console.log("FAILURE TO CONNECT SHOULD RESET SECONDS")
+        if (reconnecting) {
+          // If you're already reconnecting, you don't want to double up
+          console.log('skipping because reconnecting already')
+        } else if (secondsToReconnect <= 0) {
+          setReconnecting(true)
+          console.log('NOW RECONNECTING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+          connectSignalR(props.userId, dispatch).then((newConnection) => {
+            if (newConnection.state === HubConnectionState.Connected) {
+              setReconnecting(false)
+              dispatch(SignalRHubCreatedAction(newConnection))
+              dispatch(HideModalAction())
+            } else {
+              console.log("RECONNECT FAILED")
+              setReconnecting(false)
               setNextSecondsToReconnect(nextSecondsToReconnect * 2)
               setsecondsToReconnect(nextSecondsToReconnect)
-            })
-          }
-        } else if (props.connection.state === HubConnectionState.Connecting ||
-                   props.connection.state === HubConnectionState.Reconnecting) {
-          setsecondsToReconnect(secondsToReconnect - 1)
-          console.log('CONNECTING OR RECONNECTING')
-        } else {
+            }
+          }).catch(() => {
+            console.log("FAILURE TO CONNECT SHOULD RESET SECONDS")
+            setNextSecondsToReconnect(nextSecondsToReconnect * 2)
+            setsecondsToReconnect(nextSecondsToReconnect)
+            setReconnecting(false)
+          })
+        } else if (secondsToReconnect > 0) {
           setsecondsToReconnect(secondsToReconnect - 1)
         }
       },
@@ -49,7 +77,7 @@ export default function DisconnectModalView (props: { userId: string, connection
     );
     console.log('RESOLVING INTERVAL')
     return () => clearInterval(timerId);
-  }, [secondsToReconnect])
+  }, [secondsToReconnect, reconnecting])
 
   return (
     <div>
