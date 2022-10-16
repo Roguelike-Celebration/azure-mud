66d0d25b27569e395c0f127dc04fc45c610d2eab
Matt Hayes <matt@mysterycommand.com>
2022-10-16 10:12:51 -0500

    docs: add a note about 1e3

--- src/utils.ts
+++ src/utils.ts
@@ -9,6 +9,10 @@ export const nie = <T extends unknown>(_: T): void => {
  * returns 0 if you pass in a frame count less than or equal to 0 (negative or
  * zero fps doesn't make any sense)
  *
+ * n.b. `1e3` is just kind of a "neat" way to write "one thousand" ... if it
+ * feels "too clever" feel free to rewrite it as `1_000` or just `1000` (though
+ * I do like the numeric separators if we're going for absolute clarity)
+ *
  * @param frames the number of frames per second
  * @returns the number of milliseconds per frame
  */
