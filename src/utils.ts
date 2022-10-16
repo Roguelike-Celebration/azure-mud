47b29c41217d6127cf5bef3d0d5bcc6a481d4b05
Matt Hayes <matt@mysterycommand.com>
2022-10-16 10:10:11 -0500

    feat: an fps util

--- src/utils.ts
+++ src/utils.ts
@@ -3,3 +3,13 @@ import Config from './config'
 export const nie = <T extends unknown>(_: T): void => {
   throw Error('Not Implemented')
 }
+
+/**
+ * converts a frame count per second to the number of milliseconds per frame,
+ * returns 0 if you pass in a frame count less than or equal to 0 (negative or
+ * zero fps doesn't make any sense)
+ *
+ * @param frames the number of frames per second
+ * @returns the number of milliseconds per frame
+ */
+export const fps = (frames: number) => frames <= 0 ? 0 : 1e3 / frames
