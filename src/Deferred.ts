b88a65b74a136cf4effbac251eef9bd6ad878d4e
Matt Hayes <matt@mysterycommand.com>
2022-10-16 08:50:36 -0500

    refactor: don't extend Promise, expose a getter for one, formatting

--- src/Deferred.ts
+++ src/Deferred.ts
@@ -4,17 +4,27 @@
  * @see https://stackoverflow.com/questions/44728779/deferred-that-extends-promise
  * @see https://github.com/BabylonJS/Babylon.js/blob/master/packages/dev/core/src/Misc/deferred.ts
  */
-export class Deferred<T> extends Promise<T> {
+export class Deferred<T> {
+  private _promise: Promise<T>;
   private _resolve: (value: T | PromiseLike<T>) => void;
   private _reject: (reason?: any) => void;
 
   constructor () {
-    super((resolve, reject) => {
+    this._promise = new Promise((resolve, reject) => {
       this._resolve = resolve
       this._reject = reject
     })
   }
 
-  public get resolve () { return this._resolve }
-  public get reject () { return this._reject }
+  public get promise () {
+    return this._promise
+  }
+
+  public get resolve () {
+    return this._resolve
+  }
+
+  public get reject () {
+    return this._reject
+  }
 }
