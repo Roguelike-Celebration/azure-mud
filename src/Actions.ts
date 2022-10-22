69c4e6c48d92438a5d4b33a9c6dec3a7d9fca20f
Travis Moy <moytravis@gmail.com>
2022-10-21 18:13:43 -0700

    Defer presence write until after loading is done
    Otherwise it gets resolved before roomData & never properly resolves

--- src/Actions.ts
+++ src/Actions.ts
@@ -218,11 +218,15 @@ export const ConnectAction =
   (
     roomId: string,
     roomData: { [roomId: string]: Room },
+    presenceData?: { [roomId: string]: string[] },
     roomNotes?: RoomNote[]
   ): Thunk<Action, State> =>
     async (dispatch, getState) => {
       await getState().messageArchiveLoaded.promise
       dispatch(UpdatedCurrentRoomAction(roomId, roomData))
+      if (presenceData) {
+        dispatch(UpdatedPresenceAction(presenceData))
+      }
       if (roomNotes) {
         dispatch(NoteUpdateRoomAction(roomId, roomNotes))
       }
