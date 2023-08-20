9b7bb7788f756ff241d73b08b14369a4f354cfe3
MoyTW <moytravis@gmail.com>
2023-08-19 21:04:19 -0700

    Linting fixes
    otherwise my ide lights up like a Christmas tree lol

--- server/src/azureWrap.ts
+++ server/src/azureWrap.ts
@@ -1,12 +1,12 @@
-import { Context, HttpRequest } from "@azure/functions";
-import authenticate, { AuthenticationOptions } from "./authenticate";
+import { Context, HttpRequest } from '@azure/functions'
+import authenticate, { AuthenticationOptions } from './authenticate'
 import {
   EndpointFunction,
   AuthenticatedEndpointFunction,
   Result,
   isGroupMessage,
-  isPrivateMessage,
-} from "./endpoint";
+  isPrivateMessage
+} from './endpoint'
 
 /**
  * Each Azure Functions endpoint is basically a wrapper around an existing
@@ -16,7 +16,7 @@ import {
  * inputs/outputs nicely to work with Azure Functions + Azure SignalR Service.
  */
 
-export async function azureWrap(
+export async function azureWrap (
   context: Context,
   req: HttpRequest | undefined,
   fn: EndpointFunction,
@@ -25,51 +25,51 @@ export async function azureWrap(
   const result = await fn(
     { ...((req && req.body) || {}), ...extraInputs },
     context.log
-  );
-  outputToAzure(context, req, result);
+  )
+  outputToAzure(context, req, result)
 }
 
-export async function authenticatedAzureWrap(
+export async function authenticatedAzureWrap (
   context: Context,
   req: HttpRequest,
   fn: AuthenticatedEndpointFunction,
   opts: AuthenticationOptions = {}
 ) {
   await authenticate(context, req, opts, async (user) => {
-    const result = await fn(user, req.body || {}, context.log);
-    outputToAzure(context, req, result);
-  });
+    const result = await fn(user, req.body || {}, context.log)
+    outputToAzure(context, req, result)
+  })
 }
 
-function outputToAzure(context: Context, req: HttpRequest, result: Result) {
-  context.log("outputting to azure");
-  context.log("context: ", JSON.stringify(context));
-  context.log("result: ", JSON.stringify(result));
+function outputToAzure (context: Context, req: HttpRequest, result: Result) {
+  context.log('outputting to azure')
+  context.log('context: ', JSON.stringify(context))
+  context.log('result: ', JSON.stringify(result))
   if (result.messages) {
     context.bindings.actions = result.messages.map((m) => {
-      const actionJson = JSON.stringify({ type: m.target, value: m.arguments });
+      const actionJson = JSON.stringify({ type: m.target, value: m.arguments })
       if (isPrivateMessage(m)) {
         return {
-          actionName: "sendToUser",
+          actionName: 'sendToUser',
           userId: m.userId,
           data: actionJson,
-          dataType: "text",
-        };
+          dataType: 'text'
+        }
       } else if (isGroupMessage(m)) {
         return {
-          actionName: "sendToGroup",
+          actionName: 'sendToGroup',
           group: m.groupId,
           data: actionJson,
-          dataType: "text",
-        };
+          dataType: 'text'
+        }
       } else {
         return {
-          actionName: "sendToAll",
+          actionName: 'sendToAll',
           data: actionJson,
-          dataType: "text",
-        };
+          dataType: 'text'
+        }
       }
-    });
+    })
   }
 
   if (result.groupManagementTasks) {
@@ -80,25 +80,25 @@ function outputToAzure(context: Context, req: HttpRequest, result: Result) {
         // It's only called from 'connect'.
         // We'd want make connect return a 'removeAll' and then an 'add' for the current room
         // If we need to persist the SignalR path, this file can call Redis to find room IDs
-        let actionName = "";
-        if (t.action === "add") actionName = "addUserToGroup";
-        if (t.action === "remove") actionName = "removeUserFromGroup";
+        let actionName = ''
+        if (t.action === 'add') actionName = 'addUserToGroup'
+        if (t.action === 'remove') actionName = 'removeUserFromGroup'
         return {
           actionName,
           userId: t.userId,
-          group: t.groupId,
-        };
+          group: t.groupId
+        }
       })
-    );
+    )
   }
 
-  console.log("actions", JSON.stringify(context.bindings.actions));
+  console.log('actions', JSON.stringify(context.bindings.actions))
 
   // TimerTriggers don't have a res obj. This seems like an okay, if imperfect, guard.
   if (context.res) {
     context.res.status =
-      (result.httpResponse && result.httpResponse.status) || 200;
+      (result.httpResponse && result.httpResponse.status) || 200
     context.res.body =
-      result.httpResponse && JSON.stringify(result.httpResponse.body);
+      result.httpResponse && JSON.stringify(result.httpResponse.body)
   }
 }
