fc661e1c91347fd8cd9973c7ba826a1c20bb1280
Emilia Lazer-Walker <me@lazerwalker.com>
2024-09-14 13:02:01 -0700

    Fix azure sendMagicEmail auth

--- server/sendMagicEmail/index.ts
+++ server/sendMagicEmail/index.ts
@@ -1,13 +1,12 @@
 import { AzureFunction, Context, HttpRequest } from '@azure/functions'
-import { authenticatedAzureWrap } from '../src/azureWrap'
+import { azureWrap } from '../src/azureWrap'
 import sendMagicEmail from '../src/endpoints/sendMagicEmail'
 
 const httpTrigger: AzureFunction = async function (
   context: Context,
   req: HttpRequest
 ): Promise<any> {
-  context.log('In connect')
-  await authenticatedAzureWrap(context, req, sendMagicEmail, { audit: true })
+  await azureWrap(context, req, sendMagicEmail, { audit: true })
 }
 
 export default httpTrigger
