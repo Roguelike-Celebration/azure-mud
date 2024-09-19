35497d73666432292fbd8ada0c44f22876d87fe5
Emilia Lazer-Walker <me@lazerwalker.com>
2024-09-19 12:19:22 -0700

    Update admin panel with new flow

--- src/admin/components/LoggedOutView.tsx
+++ src/admin/components/LoggedOutView.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { sendMagicEmail } from '../../networking'
+import '../../../style/loggedOut.css'
 
 export default function LoggedOutView () {
   return (
-    <div>
+    <div className='logged-out'>
       <header role="banner">
         <h1>Welcome to the secret Roguelike Celebration backstage area!</h1>
       </header>
@@ -10,7 +12,7 @@ export default function LoggedOutView () {
         <p>
           If you&apos;re not a Roguelike Celebration admin, this isn&apos;t for you. If you should be an admin, but can&apos;t log in, ask in our other chat.
         </p>
-      TODO: Put an email input here
+        <p>If you are an admin, log into the normal social space, then come back here.</p>
       </main>
     </div>
   )
