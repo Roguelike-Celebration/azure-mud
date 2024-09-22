325d344dae48e143a87dc92c869f5fb0d9ead392
Emilia Lazer-Walker <me@lazerwalker.com>
2024-09-22 13:52:31 -0700

    Fix orb ponderer 500 error

--- server/startPonderingOrbs/index.ts
+++ server/startPonderingOrbs/index.ts
@@ -2,7 +2,6 @@ import { AzureFunction, Context, HttpRequest } from '@azure/functions'
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest, connection): Promise<void> {
   context.log('pondering orbs')
-  context.log('userid and secret', req.headers.userid, process.env.ORB_PONDER_USER)
   if (req.headers.userid !== process.env.ORB_PONDER_USER) {
     context.res = {
       status: 401,
@@ -11,13 +10,14 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     return
   }
 
-  context.log('got a real ponderer')
+  context.log('got a real pondererr')
   // This never gets undone, but the fact this group always exists is ~fine~
-  context.bindings.actions.push({
+  context.bindings.actions = [{
     actionName: 'addUserToGroup',
     userId: req.headers.userid,
     group: 'orbMessages'
-  })
+  }]
+
   context.log('Set up action to add user to group')
   context.log('connection', connection)
 
