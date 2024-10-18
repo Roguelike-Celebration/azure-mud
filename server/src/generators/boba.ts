cb48b2a948a96b87f6e3215c14ca43ca786f3792
Kawa <kawaii.dragoness@gmail.com>
2024-10-18 17:04:15 -0600

    updates to boba and vegan food generators

--- server/src/generators/boba.ts
+++ server/src/generators/boba.ts
@@ -9,7 +9,39 @@ export const actionString = (boba: string) => {
 export const generate = () => {
   var grammar = tracery.createGrammar({
     origin: [
-      "You drink a refreshing #powerup# potion with #boba#."
+      "You drink #refreshing.a# #powerup# potion with #boba#."
+    ],
+    refreshing: [
+      'refreshing',
+      'tasty',
+      'delicious',
+      'lovely',
+      'incredible',
+      'delectable',
+      'wonderfully #refreshing#', // learning a few years ago that you can recurse grammars like this makes me too powerful - kawa
+      '#flavor#-flavored'
+    ],
+    flavor: [
+      'banana',
+      'orange',
+      'lychee',
+      'strawberry',
+      'mango',
+      'peach',
+      'elderberry',
+      'haskap berry',
+      'almond', 
+      'cherry',
+      'green tea',
+      'chai',
+      'oolong',
+      'black tea',
+      'ambrosia', // mostly positive but a few weirdos down here, which are all roguelike references - kawa
+      'choko',
+      'Gros Michel banana',
+      'slime mold',
+      'cheap imitation Amulet of Yendor',
+      'starapple'
     ],
     powerup: [
       'blindness',
@@ -26,8 +58,16 @@ export const generate = () => {
       'raise level',
       'restore strength',
       'see invisible',
+      'torment',
+      'brilliance',
+      'Amulet seeking',
+      'Speak With Eels',
       'intoxication',
       'fire breath',
+      'ice breath',
+      'entirely normal breath',
+      'levitation',
+      'enlightenment',
       'brown',
       'red',
       'white',
@@ -35,7 +75,8 @@ export const generate = () => {
       'green',
       'yellow',
       'purple',
-      'orange'
+      'orange',
+      'octarine'
     ],
     boba: [
       'Tapioca Pearls',
@@ -44,12 +85,15 @@ export const generate = () => {
       'Popping Crystals',
       'Ponderous Orbs',
       'Frog\'s Eyes',
+      'Jester\'s Bells',
       'Newt\'s Eyes',
       'Lychee Jelly',
       'Stinky Durian Jelly',
       'Dragonfruit Jelly',
       'Strawberry Jelly',
-      'Eel Jelly'
+      '#flavor# bits',
+      'Eel Jelly',
+      'whipped cream and #boba#'
     ]
   })
 
