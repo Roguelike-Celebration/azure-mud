ca62f4da27a1e4a01683c41cbaa08ec42f7bfca6
BaldSavant <nathanielsavant@gmail.com>
2025-08-29 10:56:57 -0700

    Updating Map data with first pass on 2025 content

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -36,11 +36,11 @@ import _, { head, isUndefined } from 'lodash'
  }
 
   interface ClickableArea {
-    x: number,
-    y: number,
-    height: number,
-    width: number,
-    roomId: string
+	x: number,
+	y: number,
+	height: number,
+	width: number,
+	roomId: string
   }
 
 export default function MapView (props: Props) {
@@ -52,35 +52,35 @@ export default function MapView (props: Props) {
   // Pixel size of one ASCII character
   let w; let h = 0
   if (props.isMiniMap) {
-    w = 8
-    h = 13
+	w = 8
+	h = 13
   } else if (preWidth !== 0 && preHeight !== 0) {
-    w = preWidth / 121
-    h = preHeight / 37
+	w = preWidth / 121
+	h = preHeight / 37
   }
 
   // Scroll to make sure that the user's location is visible
   // The empty array at the end means we only run this on first render, not every time it re-renders
   // This ensures it only scrolls on load, not every time new presence data comes in
   React.useEffect(() => {
-    // console.log('Attempting to scroll', `${props.isMiniMap ? 'minimap-' : ''}clickable-room-${currentRoomId}`)
-    const location = document.getElementById(`${props.isMiniMap ? 'minimap-' : ''}clickable-room-${currentRoomId}`)
-    if (location) {
-      // console.log(location)
-      location.scrollIntoView({ block: 'center', inline: 'center' })
-    } else {
-      // console.log('NO LOCATION')
-    }
+	// console.log('Attempting to scroll', `${props.isMiniMap ? 'minimap-' : ''}clickable-room-${currentRoomId}`)
+	const location = document.getElementById(`${props.isMiniMap ? 'minimap-' : ''}clickable-room-${currentRoomId}`)
+	if (location) {
+	  // console.log(location)
+	  location.scrollIntoView({ block: 'center', inline: 'center' })
+	} else {
+	  // console.log('NO LOCATION')
+	}
 
-    const pre = document.getElementById('map-pre')
-    if (pre) {
-      if (preWidth !== pre.clientWidth) {
-        setPreWidth(pre.clientWidth)
-      }
-      if (preHeight !== pre.clientHeight) {
-        setPreHeight(pre.clientHeight)
-      }
-    }
+	const pre = document.getElementById('map-pre')
+	if (pre) {
+	  if (preWidth !== pre.clientWidth) {
+		setPreWidth(pre.clientWidth)
+	  }
+	  if (preHeight !== pre.clientHeight) {
+		setPreHeight(pre.clientHeight)
+	  }
+	}
   }, (props.isMiniMap ? null : []))
 
   if (!presenceData) { return <div/> }
@@ -88,297 +88,250 @@ export default function MapView (props: Props) {
   let map = mapText
 
   presenceMapping.forEach((roomId, idx) => {
-    let replaceString = '[0]'
+	let replaceString = '[0]'
 
-    // Because 0 is falsy, we need to explicitly use isUndefined
-    if (presenceData[roomId] && !isUndefined(presenceData[roomId])) {
-      replaceString = `[${presenceData[roomId]}]`
-    }
-    replaceString = replaceString.padEnd(4, '.')
-    map = map.replace(`(${idx.toString().padStart(2, '0')})`, replaceString)
+	// Because 0 is falsy, we need to explicitly use isUndefined
+	if (presenceData[roomId] && !isUndefined(presenceData[roomId])) {
+	  replaceString = `[${presenceData[roomId]}]`
+	}
+	replaceString = replaceString.padEnd(4, '.')
+	map = map.replace(`(${idx.toString().padStart(2, '0')})`, replaceString)
   })
 
   map = map.replace(/\[([0-9]*)\]/g, '($1)')
 
   const handleClick = (e) => {
-    const roomId =
-      e.target && e.target.getAttribute && e.target.getAttribute('data-room')
-    if (roomId) {
-      moveToRoom(roomId)
-      dispatch(HideModalAction())
-    }
+	const roomId =
+	  e.target && e.target.getAttribute && e.target.getAttribute('data-room')
+	if (roomId) {
+	  moveToRoom(roomId)
+	  dispatch(HideModalAction())
+	}
   }
 
   let clickableDivs = []
   if (w !== 0 && h !== 0) {
-    clickableDivs = clickableAreas.map(a => {
-      return <div
-        style={{
-          position: 'absolute',
-          left: `${a.x * w}px`,
-          top: `${a.y * h}px`,
-          width: `${a.width * w}px`,
-          height: `${a.height * h}px`,
-          cursor: 'pointer'
-        }}
-        key={a.roomId}
-        onClick={handleClick}
-        data-room={a.roomId}
-        id={`${props.isMiniMap ? 'minimap-' : ''}clickable-room-${a.roomId}`} />
-    })
+	clickableDivs = clickableAreas.map(a => {
+	  return <div
+		style={{
+		  position: 'absolute',
+		  left: `${a.x * w}px`,
+		  top: `${a.y * h}px`,
+		  width: `${a.width * w}px`,
+		  height: `${a.height * h}px`,
+		  cursor: 'pointer'
+		}}
+		key={a.roomId}
+		onClick={handleClick}
+		data-room={a.roomId}
+		id={`${props.isMiniMap ? 'minimap-' : ''}clickable-room-${a.roomId}`} />
+	})
   }
 
   return <div className='map' style={{ position: 'relative', margin: '15px' }}>
-    {clickableDivs}
-    <pre
-      id={`${props.isMiniMap ? 'minimap-' : ''}map-pre`}
-      style={{ letterSpacing: '2px', margin: '0px', width: 'fit-content' }}
-    >
-      <code>{map}</code>
-    </pre>
+	{clickableDivs}
+	<pre
+	  id={`${props.isMiniMap ? 'minimap-' : ''}map-pre`}
+	  style={{ letterSpacing: '2px', margin: '0px', width: 'fit-content' }}
+	>
+	  <code>{map}</code>
+	</pre>
   </div>
 }
 
-const mapText = `┌────────────────────────┐┌─────────────┐ ┌─────────────┐ ┌─────────────┐┌─────────┐ ~~~~~~~~~~~~~~~~~~~~
-│........................││..Bawdy......│ │.............│ │.Joust.Arena.││.........│ ~~~~~~~~~~~~~~~~~~~~~
-│┌────────┐....┌────────┐││..Singers....###.............###....(17).....││.........│  ~~~~~~~~~~~~~~~~~~~~~~~
-││.Vegan..│....│..Taco..│││....(15).....│ │.............│ │.............││.........│     ~~~~~~~~~~~~~~~~~~~~
-││.Truck..│....│.Truck..│││.............│ │.............│ │.............││.........│         ~~~~~~~~~~~~~~~~~~
-││..(00)..│....│..(01)..│││=============│ │...Theater...│ │=============││.........│           ~~~~~~~~~~~~~~~~~~
-│└────────┘....└────────┘││..Whip.Show..│ │....(13).....│ │..Fountain...││.........│               ~~~~~~~~~~~~~~~~
-│........................││....(16).....###.............###....(14).....││.........│               ~~~~~~~~~~~~~~~~~
-│........................││.............│ │.............│ │.............││.Archery.│                ~~~~~~~~~~~~~~~~~
-│┌────────┐....┌────────┐││.............│ │.............│ │.............││..(21)...│┌─────────────┐    ~~~~~~~~~~~~~~~
-││.Boba...│....│.Kebab..││└─────────────┘ └──────##─────┘ └─────────────┘│.........││.............│     ~~~~~~~~~~~~~~~
-││.Truck..│....│.Truck..││                       ##                      │.........││.............│       ~~~~~~~~~~~~~
-││..(02)..│....│..(03)..││                       ##                      │.........││..Wood Shop..│┌───────┐~~~~~~~~~~~
-│└────────┘....└────────┘│      /────────────────##───────────────\\      │.........││....(24).....││.......│~~~~~~~~~~~
-│................┌──────┐│     /┌──────────────┐.##................\\     │.........││.............││.......│~~~~~~~~~~~
-│┌───────┐.......│.Swag ││    /.│.Tattoo.Truck.│.##..┌────────────┐.\\    │.........││.............││.......└~~~~~~~~~~~
-││.......│.......│.Store││   /..│.....(11).....│.##..│..Note.Wall.│..\\   └────#────┘└──────#──────┘│...........│~~~~~~~
-││.Steam.│.......│.(19).││  /...│..............│.##..│....(12)....│...\\  ┌────#────────────#──────┐│...........│~~~~~~~
-││.Sale..│.......│......││ /....└────────#─────┘.##..└──────#─────┘....\\ │........................││.......┌~~~~~~~~~~~
-││..(18).│.......└──────┘│ │.............###############....#..........│ │........................││.......│~~~~~~~~~~~
-││.......│...............│ │.............##┌─────────┐##....#..........│ │.....Unconferencing.....││.Docks.└~~~~~~~~~~~
-││.......│..Night.Market.│ │.............##│.........│##....#..........│ │..........(20)..........││..(23).....│~~~~~~~
-││.......│......(04).....##################│.Obelisk.│####################........................##...........│~~~~~~~
-││.......│...............##################│...(09)..│####################........................││.......┌~~~~~~~~~~~
-││.......│.┌────────────┐│ │.............##└─────────┘......#..........│ │........................││.......│~~~~~~~~~~~
-││.......│.│..Standing..││ │.............#########┌─────────#────┐.....│ │........................││.......└~~~~~~~~~~~
-│└───────┘.│.. Tables ..││ │.............#########│..............│.....│ │........................││...........│~~~~~~~
-│┌─────\\ ..│....(06)....││ │....................##│..............│.....│ └────────────#───────────┘│...........│~~~~~~~
-││......\\..│............││ \\.....Courtyard......###..Pin.Vendor..│...../ ┌────────────#───────────┐│.......┌~~~~~~~~~~~
-││.......\\.└────────────┘│  \\.......(08)........##│.....(10).....│..../  │........................││.......│~~~~~~~~~~~
-││........\\─────────────┐│   \\..................##│..............│.../   │....Weapons Training....│└───────┘~~~~~~~~~~~
-││...Picnic Grounds.....││    \\.................##│..............│../    │..........(22)..........│        ~~~~~~~~~~~~
-││........(05)..........││     \\................##└──────────────┘./     │........................│      ~~~~~~~~~~~~~~
-││......................││      \\─────────────────────────────────/      └────────────────────────┘      ~~~~~~~~~~~~~~
-│└──────────────────────┘│                  ░░░░░░░░░░                                                   ~~~~~~~~~~~~~~
-└────────────────────────┘                                                                               ~~~~~~~~~~~~~~`
+const mapText = `								 ┌───────────────────┬──────┬──────────────┐
+								│...................│......│..............│
+								│.....Dressing......│Props.│..Break Room .│
+								│.......Rooms.......│Closet│.....(18).....│┌──────────┐
+								│.......(16)........│.(17).│..............││..........│
+								└────────────##─────┴──##──┴──────##──────┘│Under.The.│
+										/────##────────##─────────##────\  │..Stage...│
+									   /.................................\ │...(19)...│
+									  /...................................\│..........│
+									  │...................................│└──────────┘
+									  │...................................│
+									  │.............Big.Top...............│
+				 ┌────────────────┐	  │...............(05)................│
+				 │................│	  │...................................│
+┌──────────────┐ │................│	  │...................................│		┌───────────┐
+│..............│ │..Magic Castle..│	  \.................................../		│...........│ ┌─────────────────┐
+│..............│ │......(12)......│	   \................................./		│.Weird Lil.│ │.................│
+│.Shell Games..│ │................│		\──────────────##───────────────/		│.Guys Zoo .│ │...Strong Man....│
+│.....(13).....│ │................│	 /─────────────────##──────────────────\	│...(07)....│ │.....Tower.......│
+│............### └───────##───────┘ /..............┌───##───┐...............\	│...........│ │......(08).......│
+└─────────────##.........##....... /...............│........│................\	└##─────────┘ │.................│
+┌────────────┐.#################../................│Obelisk.│.................\	 ##...........└─────#───────────┘
+│............│.#.........##....#./.................│..(02)..│..................\ ##.Unconferencing..# ┌─────────┐
+│............│.#...............#.│....┌──────┐.....│........│...┌───────────┐..│ ##...Menagerie.....# │.........│
+│MerryGoRound│.#.Carnival Games#.│....│Future│.....└───##───┘...│..Sami's...│..│ ##......(06).......# │Clown Pit│
+│....(14)....│.#......(11).....#######│.Hut..│.........##.......│.Souvenirs.│..│ ##.................# │...(09)..│
+│............###.................│...#│.(03).│.........##########...(04)....######################### │.........│
+│............│.#####.............│...#└──#───┘....#######.......│...........│..│	 ┌─────#──────┐	  │.........│
+└────────────┘....##.............\...######.......#.............└───────────┘../	 │............│	  └─────────┘
+   ┌──────────────##─────────────┐\.......#.......#.Pavilion................../		 │............│
+   │...........Hall.Of...........│ \......#########...(01).................../		 │.Red Velvet.│
+   │...........Mirrors...........│	\.............####....................../		 │....(10)....│
+   │.............(15)............│	 \────────────────####─────────────────/		 │............│
+   │.............................│					  ####							 │............│
+   └─────────────────────────────┘					  ####							 └────────────┘				 `
 
 const presenceMapping = [
-  'veganTruck',
-  'tacoTruck',
-  'bobaTruck',
-  'kebabTruck',
-  'nightMarket',
-  'picnicGrounds',
-  'standingTables',
-  'entryway',
-  'courtyard',
+  'ticketBooth',
+  'pavilion',
   'obelisk',
-  'pinVendor',
-  'tattooTruck',
-  'noteWall',
-  'theater',
-  'fountain',
-  'bawdySingers',
-  'whipShow',
-  'joust',
-  'steamSale',
-  'swag',
+  'futureHut',
+  'souvenirs',
+  'bigTop',
   'unconferencingHub',
-  'unconfArchery',
-  'unconfWeaponsTraining',
-  'unconfDocks',
-  'unconfWoodShop'
+  'unconfLilGuys',
+  'unconfStrongMan',
+  'unconfClownPit',
+  'unconfRedVelvet',
+  'carnivalGames',
+  'magicCastle',
+  'shellGames',
+  'merryGoRound',
+  'hallOfMirrors',
+  'dressingRooms',
+  'propsCloset',
+  'breakRoom',
+  'underTheStage'
+
 ]
 
 const clickableAreas: ClickableArea[] = [
 
   {
-    roomId: 'veganTruck',
-    x: 1,
-    y: 2,
-    width: 10,
-    height: 4
+	roomId: 'pavilion',
+	x: 47,
+	y: 26,
+	width: 18,
+	height: 7
   },
   {
-    roomId: 'tacoTruck',
-    x: 15,
-    y: 2,
-    width: 10,
-    height: 4
+	roomId: 'obelisk',
+	x: 52,
+	y: 20,
+	width: 10,
+	height: 6
   },
   {
-    roomId: 'bobaTruck',
-    x: 1,
-    y: 9,
-    width: 10,
-    height: 4
+	roomId: 'futureHut',
+	x: 39,
+	y: 24,
+	width: 8,
+	height: 5
   },
   {
-    roomId: 'kebabTruck',
-    x: 15,
-    y: 9,
-    width: 10,
-    height: 4
+	roomId: 'souvenirs',
+	x: 65,
+	y: 24,
+	width: 13,
+	height: 6
   },
   {
-    roomId: 'nightMarket',
-    x: 10,
-    y: 20,
-    width: 45,
-    height: 17
+	roomId: 'bigTop',
+	x: 39,
+	y: 7,
+	width: 35,
+	height: 11
   },
   {
-    roomId: 'picnicGrounds',
-    x: 1,
-    y: 30,
-    width: 25,
-    height: 4
+	roomId: 'unconferencingHub',
+	x: 84,
+	y: 23,
+	width: 17,
+	height: 5
   },
   {
-    roomId: 'standingTables',
-    x: 11,
-    y: 24,
-    width: 14,
-    height: 5
+	roomId: 'unconfLilGuys',
+	x: 81,
+	y: 15,
+	width: 13,
+	height: 7
   },
   {
-    roomId: 'entryway',
-    x: 51,
-    y: 19,
-    width: 14,
-    height: 5
+	roomId: 'unconfStrongMan',
+	x: 95,
+	y: 17,
+	width: 18,
+	height: 7
   },
   {
-    roomId: 'courtyard',
-    x: 26,
-    y: 24,
-    width: 22,
-    height: 7
+	roomId: 'unconfClownPit',
+	x: 103,
+	y: 23,
+	width: 11,
+	height: 7
   },
   {
-    roomId: 'obelisk',
-    x: 43,
-    y: 20,
-    width: 16,
-    height: 4
+	roomId: 'unconfRedVelvet',
+	x: 86,
+	y: 28,
+	width: 14,
+	height: 8
   },
   {
-    roomId: 'pinVendor',
-    x: 50,
-    y: 25,
-    width: 7,
-    height: 16
+	roomId: 'carnivalGames',
+	x: 17,
+	y: 23,
+	width: 15,
+	height: 7
   },
   {
-    roomId: 'noteWall',
-    x: 53,
-    y: 15,
-    width: 14,
-    height: 4
+	roomId: 'magicCastle',
+	x: 18,
+	y: 13,
+	width: 18,
+	height: 8
   },
   {
-    roomId: 'tattooTruck',
-    x: 32,
-    y: 14,
-    width: 16,
-    height: 4
+	roomId: 'shellGames',
+	x: 1,
+	y: 15,
+	width: 15,
+	height: 7
   },
   {
-    roomId: 'theater',
-    x: 42,
-    y: 0,
-    width: 15,
-    height: 10
+	roomId: 'merryGoRound',
+	x: 1,
+	y: 22,
+	width: 13,
+	height: 8
   },
   {
-    roomId: 'fountain',
-    x: 57,
-    y: 5,
-    width: 15,
-    height: 5
+	roomId: 'hallOfMirrors',
+	x: 4,
+	y: 31,
+	width: 20,
+	height: 6
   },
   {
-    roomId: 'bawdySingers',
-    x: 26,
-    y: 0,
-    width: 15,
-    height: 5
+	roomId: 'dressingRooms',
+	x: 33,
+	y: 1,
+	width: 19,
+	height: 6
   },
   {
-    roomId: 'whipShow',
-    x: 26,
-    y: 5,
-    width: 15,
-    height: 5
+	roomId: 'propsCloset',
+	x: 53,
+	y: 1,
+	width: 6,
+	height: 6
   },
   {
-    roomId: 'joust',
-    x: 57,
-    y: 0,
-    width: 15,
-    height: 5
+	roomId: 'breakRoom',
+	x: 61,
+	y: 1,
+	width: 14,
+	height: 6
   },
   {
-    roomId: 'steamSale',
-    x: 1,
-    y: 15,
-    width: 9,
-    height: 11
-  },
-  {
-    roomId: 'swag',
-    x: 17,
-    y: 14,
-    width: 8,
-    height: 5
-  },
-  {
-    roomId: 'unconferencingHub',
-    x: 74,
-    y: 17,
-    width: 26,
-    height: 10
-  },
-  {
-    roomId: 'unconfArchery',
-    x: 74,
-    y: 0,
-    width: 11,
-    height: 16
-  },
-  {
-    roomId: 'unconfWeaponsTraining',
-    x: 74,
-    y: 28,
-    width: 26,
-    height: 5
-  },
-  {
-    roomId: 'unconfDocks',
-    x: 100,
-    y: 12,
-    width: 9,
-    height: 18
-  },
-  {
-    roomId: 'unconfWoodShop',
-    x: 85,
-    y: 9,
-    width: 15,
-    height: 7
+	roomId: 'underTheStage',
+	x: 76,
+	y: 4,
+	width: 11,
+	height: 7
   }
 ]
