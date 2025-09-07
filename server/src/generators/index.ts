f10adc2203486adb09f822df388094ac1eb3ded6
BaldSavant <nathanielsavant@gmail.com>
2025-09-07 13:25:17 -0700

    fixing the polymorph function by adding it back into the generators index

--- server/src/generators/index.ts
+++ server/src/generators/index.ts
@@ -1,44 +1,16 @@
-import * as vendingMachineFood from './vendingMachineFood'
-import * as fortuneCookies from './fortuneCookies'
+import * as madamChrysalia from './fortuneTellerNPC'
+import * as dromadTam from './souvenirNPC'
 import * as polymorph from './polymorph'
-import * as motivationPosters from './motivationPosters'
-import * as closedSigns from './closedSigns'
-import * as gameRecommendations from './gameRecs'
-import * as robots from './robots'
-import * as pickupLines from './pickupLines'
-import * as ubizaraTheBartender from './ubizaraTheBartender'
-import * as doctorHope from './doctorHope'
-import * as chadSilverbow from './chadSilverbow'
-import * as zeroCrash from './zeroCrash'
-import * as randorTheTwisted from './randorTheTwisted'
-import * as drinkSkeletons from './drinkSkeletons'
-import * as drinkVessels from './drinkVessels'
-import * as drinkContents from './drinkContents'
-import * as drinkNames from './drinkNames'
 
 interface Generator {
-    generate(): string
-    actionString(item: string): string
+  generate(): string
+  actionString(item: string): string
 }
 
 const generators: {[name: string]: Generator} = {
-  vendingMachineFood,
-  fortuneCookies,
-  polymorph,
-  motivationPosters,
-  closedSigns,
-  gameRecommendations,
-  robots,
-  pickupLines,
-  ubizaraTheBartender,
-  doctorHope,
-  chadSilverbow,
-  zeroCrash,
-  randorTheTwisted,
-  drinkSkeletons,
-  drinkVessels,
-  drinkContents,
-  drinkNames
+  madamChrysalia,
+  dromadTam,
+  polymorph
 }
 
 export default generators
