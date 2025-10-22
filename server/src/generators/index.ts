f996a6acfb92934129c06120a5a4cbc253070476
BaldSavant <nathanielsavant@gmail.com>
2025-10-22 15:03:09 -0700

    forgot to actually import those generator files cause I am TIRED, yall...

--- server/src/generators/index.ts
+++ server/src/generators/index.ts
@@ -1,6 +1,14 @@
 import * as madamChrysalia from './fortuneTellerNPC'
 import * as dromadTam from './souvenirNPC'
 import * as polymorph from './polymorph'
+import * as balloonAnimals from './balloonAnimals'
+import * as popcorn from './popcorn'
+import * as snowCone from './snowCone'
+import * as deepFriedSnacks from './deepFriedSnacks'
+import * as obeliskSouvenirs from './obeliskSouvenirs'
+import * as barathrumites from './barathrumites'
+import * as mimeKing from './mimeKing'
+import * as tarotPull from './tarotPull'
 
 interface Generator {
   generate(): string
