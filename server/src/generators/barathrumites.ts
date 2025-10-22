8318da9579be7ae7af26c00d31908c0b4edf9a8f
BaldSavant <nathanielsavant@gmail.com>
2025-10-22 15:23:43 -0700

    more space operations for a couple of missed files

--- server/src/generators/barathrumites.ts
+++ server/src/generators/barathrumites.ts
@@ -10,9 +10,9 @@ export const generate = () => {
   var grammar = tracery.createGrammar({
     origin: [
       'They ask if you have yet gone to retrieve the waydroid.',
-	  'They lean forward conspiratorially and ask if you have a willing spirit.',
-	  'They suggest you should /go to the rust caves north of Grit Gate.',
-	  'Their quills shift around urshiib smiles and their leader steps forward speaking the words Live and Drink.' 
+      'They lean forward conspiratorially and ask if you have a willing spirit.',
+      'They suggest you should /go to the rust caves north of Grit Gate.',
+      'Their quills shift around urshiib smiles and their leader steps forward speaking the words Live and Drink.'
     ]
   })
 
